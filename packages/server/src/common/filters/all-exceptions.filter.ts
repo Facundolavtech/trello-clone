50816730fe3603ee9db83b52b163d79a1b45b194
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-20 14:05:54 -0300

    fix(server): improve error message for unexpected errors on AllExceptionsFilter

---
+++ packages/server/src/common/filters/all-exceptions.filter.ts
@@ -0,0 +1,18 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response: Response = ctx.getResponse();
+    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    const message = exception.sqlMessage || exception.response || exception;
+
+    return response.status(status).json({
+      statusCode: status,
+      message: status === 500 ? 'An unexpected error occurred on our servers' : message.message,
+      timestamp: new Date(Date.now()),
+    });
+  }
+}
