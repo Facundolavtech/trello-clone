f91b84112f6e3588e804a741bdfdde101009ba1d
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-20 14:28:35 -0300

    chore(server): update bootstrap process and middleware configuration

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,30 +1,74 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
-import { AllExceptionsFilter } from './core/filters/all-exceptions.filter';
+import { SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import * as connectRedis from 'connect-redis';
+import * as morgan from 'morgan';
+import * as dotenv from 'dotenv';
+import helmet from 'helmet';
+import { NODE_ENV } from './constants';
+import { AppModule } from './app.module';
+import redisClient from './config/redis.config';
+import swaggerConfig from './config/swagger.config';
+import config from './config';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const { globalPrefix, port, environment, domain, auth, client } = config();
+
+  app.setGlobalPrefix(globalPrefix);
+
+  if (environment === NODE_ENV.PRODUCTION) {
+    app.set('trust proxy', 1);
+  }
+
+  if (environment === NODE_ENV.DEVELOPMENT) {
+    app.use(morgan('tiny'));
+  }
+
+  SwaggerModule.setup(globalPrefix, app, swaggerConfig({ app }));
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  const port: number = Number(process.env.PORT) || 8000;
+  app.enableCors({ origin: client.baseURL, credentials: true });
+  app.use(helmet());
 
-  const config = new DocumentBuilder()
-    .setTitle('Thullo API Documentation')
-    .setDescription('API Documentation for Thullo API')
-    .setVersion('1.0')
-    .addTag('thullo')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(port).then(() => {
-    Logger.log(`Server running on port ${port}`);
-  });
+  const RedisStore = connectRedis(session);
+
+  await redisClient.connect();
+
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      name: auth.session.cookieName,
+      cookie: {
+        maxAge: 60000 * 60 * 24 * 7,
+        domain: environment === NODE_ENV.PRODUCTION ? domain : undefined,
+        httpOnly: true,
+        secure: environment === NODE_ENV.PRODUCTION,
+      },
+      secret: auth.session.secret,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(cookieParser());
+
+  await app.listen(port);
+
+  Logger.log(`Application is running on ${environment.toUpperCase()} mode at ${await app.getUrl()}`);
 }
 
 bootstrap();
