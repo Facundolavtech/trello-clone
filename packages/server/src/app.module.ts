ca7ea4b690837fde0f9be3855b4eb3eb891f689b
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-20 14:24:40 -0300

    refactor(server): añadir módulo acelerador en producción, proveedores y módulos

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,30 +1,58 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { BoardModule } from './boards/board.module';
-import config from './config/config';
-
-import { APP_FILTER } from '@nestjs/core';
-import { AllExceptionsFilter } from './core/filters/all-exceptions.filter';
-
-import { createDatabase } from './config/database';
+import { Inject, Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { PassportModule } from '@nestjs/passport';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { NODE_ENV } from './constants';
+import { EmptyBodyInterceptor } from './common/interceptors/empty-body.interceptor';
+import config from './config';
+import { databaseConfigAsync } from './config/database.config';
+import { AuthModule } from './modules/Auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { BoardModule } from './modules/Board/board.module';
+import { RedisCacheModule } from './modules/Redis/redis.module';
 
 @Module({
   imports: [
-    createDatabase,
-    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
     AuthModule,
-    UsersModule,
+    UserModule,
     BoardModule,
+    RedisCacheModule,
+    TypeOrmModule.forRootAsync(databaseConfigAsync),
+    PassportModule.register({
+      session: true,
+    }),
   ],
-  controllers: [AuthController],
   providers: [
     {
       provide: APP_FILTER,
       useClass: AllExceptionsFilter,
     },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: EmptyBodyInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useFactory: () => ({
+        useClass: process.env.NODE_ENV === NODE_ENV.PRODUCTION ? ThrottlerGuard : null,
+      }),
+    },
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(@Inject(config.KEY) private configService: ConfigType<typeof config>) {
+    if (this.configService.environment === NODE_ENV.PRODUCTION) {
+      ThrottlerModule.forRoot({
+        ttl: 15 * 60 * 1000,
+        limit: 100,
+      });
+    }
+  }
+}
