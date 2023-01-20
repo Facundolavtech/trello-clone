d3da422970839f1dce5c02c8bcbb0327a4b2982e
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-20 14:19:32 -0300

    feat(server): add global entities module to not import entities in each module

---
+++ packages/server/src/common/entities/entities.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BaseEntity } from 'typeorm';
+import { Board } from '../../modules/Board/entities/Board.entity';
+import { BoardMember } from '../../modules/Board/entities/BoardMember.entity';
+import { BoardCard } from '../../modules/Board/modules/Card/entities/Card.entity';
+import { BoardCardAttachment } from '../../modules/Board/modules/Card/modules/Attachment/entities/Attachment.entity';
+import { BoardCardComment } from '../../modules/Board/modules/Card/modules/Comment/entities/Comment.entity';
+import { BoardCardLabel } from '../../modules/Board/modules/Card/modules/Label/entities/Label.entity';
+import { BoardList } from '../../modules/Board/modules/List/entities/BoardList.entity';
+
+import { User } from '../../modules/user/entities/User.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, BaseEntity, Board, BoardMember, BoardList, BoardCard, BoardCardAttachment, BoardCardComment, BoardCardLabel])],
+  exports: [TypeOrmModule],
+})
+export class EntitiesModule {}
