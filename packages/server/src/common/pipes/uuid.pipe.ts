582be2fac6b621d62323ae611428c26efaee3634
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-23 15:27:12 -0300

    refactor(server): translate uuid pipe message

--- packages/server/src/common/pipes/uuid.pipe.ts
+++ packages/server/src/common/pipes/uuid.pipe.ts
@@ -1,12 +1,12 @@
-import { PipeTransform, ArgumentMetadata, BadRequestException, Injectable } from '@nestjs/common';
+import { PipeTransform, BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomUUIDPipe implements PipeTransform<string> {
   private readonly uuidRegex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
 
-  async transform(value: string, metadata: ArgumentMetadata) {
+  async transform(value: string) {
     if (!this.uuidRegex.test(value)) {
-      throw new BadRequestException('Error: El tipo de ID es invalido');
+      throw new BadRequestException('Error: The ID type is invalid');
     }
     return value;
   }
