20957f854ce0c01bf3a666411ff5230f6c5faa36
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-23 15:28:01 -0300

    fix(server): user module import

--- packages/server/src/common/types/index.ts
+++ packages/server/src/common/types/index.ts
@@ -1,4 +1,4 @@
-import { User } from '../../modules/user/entities/User.entity';
+import { User } from '../../modules/User/entities/User.entity';
 
 export interface AuthenticatedRequest extends Express.Request {
   user: User;
