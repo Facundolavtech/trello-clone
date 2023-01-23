68ae844e4bc19dd5bf8d26fdcd8a1483897972b6
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-23 15:31:42 -0300

    style(server): type register dto in register strategy

--- packages/server/src/modules/Auth/strategies/register.strategy.ts
+++ packages/server/src/modules/Auth/strategies/register.strategy.ts
@@ -3,6 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { Strategy } from 'passport-custom';
 import { User } from '../../user/entities/User.entity';
+import { RegisterLocalDTO } from '../dto/auth.dto';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
@@ -12,7 +13,7 @@ export class RegisterStrategy extends PassportStrategy(Strategy, 'register') {
   }
 
   async validate(req: Request): Promise<User> {
-    const registerDTO = req.body;
+    const registerDTO: RegisterLocalDTO = req.body;
     return await this.authService.registerLocal(registerDTO);
   }
 }
