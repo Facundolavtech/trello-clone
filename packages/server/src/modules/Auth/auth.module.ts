37bbccac6b9442da4964ac1fa902c9c5f6177a96
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-23 15:30:24 -0300

    refactor(server): remove unnecesary forward ref in auth module, add user module

--- packages/server/src/modules/Auth/auth.module.ts
+++ packages/server/src/modules/Auth/auth.module.ts
@@ -1,7 +1,9 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EntitiesModule } from '../../common/entities/entities.module';
-import { UserService } from '../user/services/user.service';
+import { User } from '../user/entities/User.entity';
+import { UserService } from '../User/services/user.service';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
@@ -12,12 +14,13 @@ import { SessionSerializer } from './utils/SessionSerializer';
 
 @Module({
   imports: [
-    forwardRef(() => UserModule),
+    UserModule,
+    EntitiesModule,
+    TypeOrmModule.forFeature([User]),
     PassportModule.register({
       session: true,
       defaultStrategy: 'google',
     }),
-    EntitiesModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, UserService, LocalStrategy, RegisterStrategy, GoogleStrategy, SessionSerializer],
