7c78823fe921a304e19121366e02214d50d0867e
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-24 03:09:55 -0300

    feat(server): add exclude paths to empty body interceptor

--- packages/server/src/common/interceptors/empty-body.interceptor.ts
+++ packages/server/src/common/interceptors/empty-body.interceptor.ts
@@ -1,12 +1,17 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, BadRequestException } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+const excludePaths = ['/attachments/upload'];
 
 @Injectable()
 export class EmptyBodyInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.switchToHttp().getRequest();
+    const request: Request = context.switchToHttp().getRequest();
 
-    if (request.method === 'POST' || request.method === 'PUT') {
+    const isExcludedPath = excludePaths.some((url) => request.path.includes(url));
+
+    if ((request.method === 'POST' || request.method === 'PUT') && !isExcludedPath) {
       const { body } = request;
 
       if (!Object.keys(body).length) {
