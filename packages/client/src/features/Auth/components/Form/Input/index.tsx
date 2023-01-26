03a1c4f13b3aed913f5cf8ebe3166bee9cd1ea91
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-26 03:16:27 -0300

    feat(client): add reusable auth form input

---
+++ packages/client/src/features/Auth/components/Form/Input/index.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent, FC } from 'react';
+import { Icon, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { IconType } from 'react-icons/lib';
+
+type Props = {
+  icon: IconType;
+  type: string;
+  placeholder: string;
+  name: string;
+  value: string;
+  disabled?: boolean;
+  handleChange: (e: string | ChangeEvent<any>) => void;
+};
+
+const AuthInput: FC<Props> = ({ icon, type, placeholder, disabled = false, handleChange, name, value }) => {
+  return (
+    <InputGroup size="lg">
+      <InputLeftElement pointerEvents="none">
+        <Icon as={icon} color="gray.400" />
+      </InputLeftElement>
+      <Input
+        focusBorderColor="gray.500"
+        type={type}
+        placeholder={placeholder}
+        background="white"
+        name={name}
+        value={value}
+        disabled={disabled}
+        onChange={handleChange}
+        _placeholder={{ fontSize: 15, color: 'gray.400' }}
+      />
+    </InputGroup>
+  );
+};
+
+export default AuthInput;
