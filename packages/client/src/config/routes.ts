fb92c7a27e034777eefc0ff375a3408816ad0909
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-26 03:09:56 -0300

    feat(client): add api routes config

---
+++ packages/client/src/config/routes.ts
@@ -0,0 +1,12 @@
+export enum ApiRoutes {
+  AUTH = '/auth',
+  USER = '/user',
+  BOARD = '/board',
+}
+
+export enum AppRoutes {
+  DASHBOARD = '/dashboard',
+  LOGIN = '/login',
+  REGISTER = '/register',
+  BOARD = '/dashboard/board',
+}
