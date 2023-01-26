514e4a0ad27f95b4054760ffd3fe6a11e6f89224
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-26 03:16:07 -0300

    feat(client): add reusable auth form error message component

---
+++ packages/client/src/features/Auth/components/Form/ErrorMessage/index.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { List, ListIcon, ListItem } from '@chakra-ui/react';
+import { MdError } from 'react-icons/md';
+import { FormikErrors } from 'formik';
+
+type Props = {
+  errors: FormikErrors<object>;
+};
+
+const ErrorMessage: FC<Props> = ({ errors }) => {
+  return (
+    <List padding={6} bg="gray.100" borderRadius={6} spacing={6}>
+      {Object.values(errors).map((value, index) => (
+        <ListItem color="red.400" fontSize={16} lineHeight="1rem" fontWeight={400} key={index} display="flex" alignItems="flex-start">
+          <ListIcon as={MdError} color="red.400" width={4} height={4} mr={4} />
+          {value}
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default ErrorMessage;
