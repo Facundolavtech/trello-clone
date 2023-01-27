d3b4e137fbce64b5fd295b68830bb37bb85e9d34
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-27 14:25:32 -0300

    feat(client): add conditional to render the board title only on the board path

--- packages/client/src/components/Dashboard/Header/index.tsx
+++ packages/client/src/components/Dashboard/Header/index.tsx
@@ -1,23 +1,41 @@
-import { Divider, HStack, Text } from '@chakra-ui/react';
+import { Divider, HStack, Text, Icon } from '@chakra-ui/react';
 import Searchbar from '../../../features/Board/components/Searchbar';
+import BoardTitle from '../../../features/Board/components/Title';
+import Button from '../../Button';
 import WrappedContainer from '../../Containers/Wrapped';
+import { CgMenuGridR } from 'react-icons/cg';
 import Logo from '../../Logo';
+import Menu from '../Menu';
+import Link from 'next/link';
+import { AppRoutes } from '../../../config/routes';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <HStack as="header" boxShadow="0px 2px 2px rgba(0, 0, 0, 0.05)" width="full" bg="white" height="68px" px={6} alignItems="center">
       <WrappedContainer styles={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <HStack spacing={16} height="full">
           <Logo withTitle width="32px" height="29px" />
-          <HStack spacing={4} height="full">
-            <Text>BoardTitle</Text>
-            <Divider orientation="vertical" height="50%" color="#E0E0E0" />
-            <Text>All boards button</Text>
-          </HStack>
+          {router.pathname.includes(AppRoutes.BOARD) && (
+            <HStack spacing={4} height="full">
+              <BoardTitle />
+              <Divider orientation="vertical" height="50%" color="#E0E0E0" />
+              <Link href={AppRoutes.DASHBOARD}>
+                <Button style={{ gap: 8 }} variant="lightgray" px={4} py={3}>
+                  <Icon as={CgMenuGridR} width="12px" height="12px" color="gray.3" />
+                  <Text color="gray.3" fontSize={12} fontWeight={500}>
+                    All boards
+                  </Text>
+                </Button>
+              </Link>
+            </HStack>
+          )}
         </HStack>
         <HStack spacing={10}>
           <Searchbar />
-          <Text>Profile</Text>
+          <Menu />
         </HStack>
       </WrappedContainer>
     </HStack>
