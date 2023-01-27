7f0638b4b77873ec76529a15637b2a50ac8be252
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-27 12:40:26 -0300

    feat(client): add board page

---
+++ packages/client/src/pages/dashboard/board/[id].tsx
@@ -0,0 +1,13 @@
+import SEO from '../../../components/SEO';
+import DashboardLayout from '../../../layout/Dashboard';
+
+const Board = () => {
+  return (
+    <>
+      <SEO title="Board" />
+      <DashboardLayout>{null}</DashboardLayout>
+    </>
+  );
+};
+
+export default Board;
