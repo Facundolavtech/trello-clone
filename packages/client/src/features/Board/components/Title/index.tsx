2585432b08140940865e894aadfaec5826ee8e18
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-27 14:25:14 -0300

    feat(client): add board title

---
+++ packages/client/src/features/Board/components/Title/index.tsx
@@ -0,0 +1,11 @@
+import { Heading } from '@chakra-ui/react';
+
+const BoardTitle = () => {
+  return (
+    <Heading color="gray.1" fontSize={18} fontWeight={500}>
+      Devchallenges Board
+    </Heading>
+  );
+};
+
+export default BoardTitle;
