d4810fd34983287c2aab38cec6866d1f85b35bb1
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-27 12:42:47 -0300

    refactor(client): change auth form error message styles

--- packages/client/src/features/Auth/components/Form/ErrorMessage/index.tsx
+++ packages/client/src/features/Auth/components/Form/ErrorMessage/index.tsx
@@ -11,7 +11,7 @@ const ErrorMessage: FC<Props> = ({ errors }) => {
   return (
     <List padding={6} bg="gray.100" borderRadius={6} spacing={6}>
       {Object.values(errors).map((value, index) => (
-        <ListItem color="red.400" fontSize={16} lineHeight="1rem" fontWeight={400} key={index} display="flex" alignItems="flex-start">
+        <ListItem color="red.400" fontSize={14} lineHeight="1rem" fontWeight={400} key={index} display="flex" alignItems="flex-start">
           <ListIcon as={MdError} color="red.400" width={4} height={4} mr={4} />
           {value}
         </ListItem>
