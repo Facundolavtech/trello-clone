451e21a18737eb1a69a9d39434feebfb7a3a0c40
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-27 14:45:21 -0300

    refactor(client): change seo og title and url

--- packages/client/src/components/SEO/index.tsx
+++ packages/client/src/components/SEO/index.tsx
@@ -13,12 +13,12 @@ const SEO: FC<Props> = ({ title }) => {
         name="description"
         content="With our platform, you can organize your tasks and projects in a visual and collaborative way. Create boards for different projects and add cards detailing specific tasks. Assign tasks to members of your team and follow the progress in real time. Plus, you can add tags and blackout dates to keep you focused and on track. Try our platform for free today and experience the efficiency of Trello organization!"
       />
-      <meta property="og:title" content="Add a Shopping Cart to Any Website in Minutes - Snipcart" />
+      <meta property="og:title" content="Organize your tasks and projects in a visual and collaborative way" />
       <meta
         property="og:description"
         content="With our platform, you can organize your tasks and projects in a visual and collaborative way. Create boards for different projects and add cards detailing specific tasks. Assign tasks to members of your team and follow the progress in real time. Plus, you can add tags and blackout dates to keep you focused and on track. Try our platform for free today and experience the efficiency of Trello organization!"
       />
-      <meta property="og:url" content="https://faketrello.vercel.app/" />
+      <meta property="og:url" content="https://flavtech-trello-challenge.vercel.app" />
       <meta property="og:type" content="website" />
     </Head>
   );
