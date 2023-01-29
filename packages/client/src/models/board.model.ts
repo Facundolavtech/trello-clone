aa720dc96970d5e9a738fe97317aa4b2b07f6b62
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-28 21:45:29 -0300

    feat(client): add board model

---
+++ packages/client/src/models/board.model.ts
@@ -0,0 +1,42 @@
+import { UserProvider } from './user.model';
+
+export interface IBoard {
+  id: string;
+  title: string;
+  cover: string;
+  description: string;
+  isPrivate: boolean;
+  admin: IBoardAdmin;
+  members: IBoardMember[];
+  createdAt: number;
+  updatedAt: number;
+}
+
+export interface IBoardAdmin {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  picture: string | null;
+  provider: UserProvider;
+  providerId: string | null;
+  createdAt: number;
+  updatedAt: number;
+}
+
+export interface IBoardMember {
+  id: string;
+  createdAt: number;
+  updatedAt: number;
+  user: {
+    id: string;
+    createdAt: number;
+    updatedAt: number;
+    provider: UserProvider;
+    providerId: string | null;
+    email: string;
+    name: string;
+    username: string;
+    picture: string | null;
+  };
+}
