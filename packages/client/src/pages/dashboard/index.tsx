16d0fe916745d9509f870f9645c18f8ec7f986d6
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-28 21:50:17 -0300

    feat(client): add boards title, new board button and render mock boards in dashboard page

--- packages/client/src/pages/dashboard/index.tsx
+++ packages/client/src/pages/dashboard/index.tsx
@@ -1,11 +1,32 @@
+import { Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
+import WrappedContainer from '../../components/Containers/Wrapped';
 import SEO from '../../components/SEO';
+import Board from '../../features/Board/components/Board';
+import NewBoardButton from '../../features/Board/components/Buttons/NewBoard';
 import DashboardLayout from '../../layout/Dashboard';
+import boards from '../../mocks/boards';
 
 const Dashboard = () => {
   return (
     <>
       <SEO title="Dashboard" />
-      <DashboardLayout>{null}</DashboardLayout>
+      <DashboardLayout>
+        <WrappedContainer>
+          <VStack spacing="40px" mt="60px" width="full">
+            <HStack width="full" justifyContent="space-between">
+              <Heading fontWeight={500} fontSize={18} color="gray.1">
+                All boards
+              </Heading>
+              <NewBoardButton />
+            </HStack>
+            <SimpleGrid width="full" gap={8} columns={{ base: 1, md: 3, lg: 4 }}>
+              {boards.map((board) => (
+                <Board key={board.id} board={board} />
+              ))}
+            </SimpleGrid>
+          </VStack>
+        </WrappedContainer>
+      </DashboardLayout>
     </>
   );
 };
