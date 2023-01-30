958baaa157f51e9706cc40ba1d405814451ea836
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-29 22:09:27 -0300

    refactor(client): change auth methods to query mutations

--- packages/client/src/features/Auth/hooks/useAuthMethods.tsx
+++ packages/client/src/features/Auth/hooks/useAuthMethods.tsx
@@ -1,31 +1,80 @@
+import { useToast } from '@chakra-ui/react';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/router';
+import { useMutation, useQuery } from 'react-query';
 import http from '../../../config/http';
-import { ApiRoutes } from '../../../config/routes';
+import { ApiRoutes, AppRoutes } from '../../../config/routes';
 import { ILoginFormValues } from '../components/Form/Login';
 import { IRegisterFormValues } from '../components/Form/Register';
 import SocialProviders from '../constants/providers';
 
 const useAuthMethods = () => {
-  const loginWithSocialProvider = async (provider: SocialProviders, access_token: string) => {
-    return await http.api.post(`${ApiRoutes.AUTH}/${provider}`, { token: access_token });
-  };
+  const router = useRouter();
+  const toast = useToast();
 
-  const loginWithLocalProvider = async (credentials: ILoginFormValues) => {
-    return await http.api.post(`${ApiRoutes.AUTH}/local/login`, credentials);
-  };
+  const loginSocialMutation = useMutation(
+    ({ provider, token }: { provider: SocialProviders; token: string }) => http.api.post(`${ApiRoutes.AUTH}/${provider}`, { token }),
+    {
+      onSuccess: () => {
+        return router.push(AppRoutes.DASHBOARD);
+      },
+      onError: (err: AxiosError<any, any>) => {
+        toast({
+          isClosable: false,
+          status: 'error',
+          title: err.response?.data.message,
+          position: 'top-right',
+          variant: 'solid',
+          duration: 1500,
+        });
+      },
+    }
+  );
 
-  const registerWithLocalProvider = async (credentials: IRegisterFormValues) => {
-    return await http.api.post(`${ApiRoutes.AUTH}/local/register`, credentials);
-  };
+  const loginLocalMutation = useMutation((credentials: ILoginFormValues) => http.api.post(`${ApiRoutes.AUTH}/local/login`, credentials), {
+    onSuccess: () => {
+      return router.push(AppRoutes.DASHBOARD);
+    },
+    onError: (err: AxiosError<any, any>) => {
+      toast({
+        isClosable: false,
+        status: 'error',
+        title: err.response?.data.message,
+        position: 'top-right',
+        variant: 'solid',
+        duration: 1500,
+      });
+    },
+  });
 
-  const logout = async () => {
-    return await http.api.get(`${ApiRoutes.AUTH}/logout`);
-  };
+  const registerLocalMutation = useMutation((credentials: IRegisterFormValues) => http.api.post(`${ApiRoutes.AUTH}/local/register`, credentials), {
+    onSuccess: () => {
+      return router.push(AppRoutes.DASHBOARD);
+    },
+    onError: (err: AxiosError<any, any>) => {
+      toast({
+        isClosable: false,
+        status: 'error',
+        title: err.response?.data.message,
+        position: 'top-right',
+        variant: 'solid',
+        duration: 1500,
+      });
+    },
+  });
+
+  const logoutQuery = useQuery('auth/logout', () => http.api.get(`${ApiRoutes.AUTH}/logout`), {
+    enabled: false,
+    onSuccess: () => {
+      return router.push(AppRoutes.LOGIN);
+    },
+  });
 
   return {
-    loginWithSocialProvider,
-    loginWithLocalProvider,
-    registerWithLocalProvider,
-    logout,
+    loginSocialMutation,
+    loginLocalMutation,
+    registerLocalMutation,
+    logout: logoutQuery.refetch,
   };
 };
 
