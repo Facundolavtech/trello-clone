f83f3416cea17797249e7f0f7996f8c0ad757787
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-29 22:13:51 -0300

    feat(client): fetch and render boards in dashboard page

--- packages/client/src/pages/dashboard/index.tsx
+++ packages/client/src/pages/dashboard/index.tsx
@@ -1,12 +1,26 @@
 import { Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
+import { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
 import WrappedContainer from '../../components/Containers/Wrapped';
 import SEO from '../../components/SEO';
+import http from '../../config/http';
+import { ApiRoutes } from '../../config/routes';
 import Board from '../../features/Board/components/Board';
 import NewBoardButton from '../../features/Board/components/Buttons/NewBoard';
+import { withSession } from '../../hoc/withSession';
 import DashboardLayout from '../../layout/Dashboard';
-import boards from '../../mocks/boards';
+import { IBoard } from '../../models/board.model';
 
 const Dashboard = () => {
+  const getBoardsQuery = useQuery('boards/all', async () => {
+    const response: AxiosResponse<IBoard[]> = await http.api.get(`${ApiRoutes.BOARD}`);
+    return response.data;
+  });
+
+  if (getBoardsQuery.isLoading) return null;
+
+  if (getBoardsQuery.error) return null;
+
   return (
     <>
       <SEO title="Dashboard" />
@@ -20,7 +34,7 @@ const Dashboard = () => {
               <NewBoardButton />
             </HStack>
             <SimpleGrid width="full" gap={8} columns={{ base: 1, md: 3, lg: 4 }}>
-              {boards.map((board) => (
+              {getBoardsQuery.data?.map((board) => (
                 <Board key={board.id} board={board} />
               ))}
             </SimpleGrid>
@@ -31,4 +45,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default withSession(Dashboard);
