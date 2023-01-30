05e07fc52f5fd34bf7a6f4559c423677691acf35
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-29 22:15:14 -0300

    feat(client): add dynamic information in dashboard components

--- packages/client/src/components/Dashboard/Menu/index.tsx
+++ packages/client/src/components/Dashboard/Menu/index.tsx
@@ -1,20 +1,28 @@
 import { Divider, Menu as ChakraMenu, MenuList, Text, VStack } from '@chakra-ui/react';
 import MenuItems from './Items';
 import MenuButton from './Button';
+import { useQuery } from 'react-query';
+import { IUser } from '../../../models/user.model';
+import formatDate from 'date-fns/format';
+import { fromUnixTime } from 'date-fns';
 
 const Menu = () => {
+  const { data } = useQuery<IUser>('user/profile');
+
+  if (!data) return null;
+
   return (
     <ChakraMenu>
       <MenuButton />
       <MenuList padding={4}>
         <VStack alignItems="flex-start">
           <Text color="gray.4" fontSize={14} fontWeight={400}>
-            flavagnino8@gmail.com
+            {data.email}
           </Text>
           <Text color="gray.4" fontSize={14} fontWeight={400}>
             Register at:{' '}
             <Text as="strong" color="gray.3" fontWeight={500}>
-              November 8 2023
+              {formatDate(fromUnixTime(data.createdAt), 'dd/MM/yyyy')}
             </Text>
           </Text>
         </VStack>
--- packages/client/src/features/Board/components/Title/index.tsx
+++ packages/client/src/features/Board/components/Title/index.tsx
@@ -1,9 +1,13 @@
 import { Heading } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
+import { IBoard } from '../../../../models/board.model';
 
 const BoardTitle = () => {
+  const { data } = useQuery<IBoard>('boards/id');
+
   return (
     <Heading color="gray.1" fontSize={18} fontWeight={500}>
-      Devchallenges Board
+      {data?.title}
     </Heading>
   );
 };
