6be330a032432388e8483fca830dd5a580cf5427
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-29 22:08:11 -0300

    fix(client): title in seo component

--- packages/client/src/components/SEO/index.tsx
+++ packages/client/src/components/SEO/index.tsx
@@ -2,13 +2,13 @@ import { FC } from 'react';
 import Head from 'next/head';
 
 type Props = {
-  title: string;
+  title?: string;
 };
 
 const SEO: FC<Props> = ({ title }) => {
   return (
     <Head>
-      <title>Thullo - {title}</title>
+      <title>{title ? `Thullo - ${title}` : 'Thullo'}</title>
       <meta
         name="description"
         content="With our platform, you can organize your tasks and projects in a visual and collaborative way. Create boards for different projects and add cards detailing specific tasks. Assign tasks to members of your team and follow the progress in real time. Plus, you can add tags and blackout dates to keep you focused and on track. Try our platform for free today and experience the efficiency of Trello organization!"
