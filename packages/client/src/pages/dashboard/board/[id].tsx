629113e0ee49235dbadcbdf32220bc27fefdc65c
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-29 22:16:46 -0300

    feat(client): fetch single board by id in board page

--- packages/client/src/pages/dashboard/board/[id].tsx
+++ packages/client/src/pages/dashboard/board/[id].tsx
@@ -1,13 +1,35 @@
+import { Text } from '@chakra-ui/react';
+import { AxiosResponse } from 'axios';
+import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
 import SEO from '../../../components/SEO';
+import http from '../../../config/http';
+import { ApiRoutes } from '../../../config/routes';
+import { withSession } from '../../../hoc/withSession';
 import DashboardLayout from '../../../layout/Dashboard';
+import { IBoard } from '../../../models/board.model';
 
 const Board = () => {
+  const router = useRouter();
+
+  const getBoardQuery = useQuery('boards/id', async () => {
+    const response: AxiosResponse<IBoard> = await http.api.get(`${ApiRoutes.BOARD}/${router.query.id}`);
+
+    return response.data;
+  });
+
+  if (getBoardQuery.isLoading) return null;
+
+  if (getBoardQuery.error) return null;
+
   return (
     <>
-      <SEO title="Board" />
-      <DashboardLayout>{null}</DashboardLayout>
+      <SEO title={getBoardQuery.data?.title} />
+      <DashboardLayout>
+        <Text>{JSON.stringify(getBoardQuery.data, null, 2)}</Text>
+      </DashboardLayout>
     </>
   );
 };
 
-export default Board;
+export default withSession(Board);
