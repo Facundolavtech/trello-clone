2b1150eebc3fffe00f996787ec98c1ab2018b7d7
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-29 22:05:48 -0300

    feat(client): add redirection in auth pages when user is logged in

--- packages/client/src/pages/login/index.tsx
+++ packages/client/src/pages/login/index.tsx
@@ -1,4 +1,7 @@
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import SEO from '../../components/SEO';
+import http from '../../config/http';
+import { ApiRoutes, AppRoutes } from '../../config/routes';
 import AuthCard from '../../features/Auth/components/Card';
 import LoginForm from '../../features/Auth/components/Form/Login';
 import AuthLayout from '../../layout/Auth';
@@ -17,3 +20,24 @@ const Login = () => {
 };
 
 export default Login;
+
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  try {
+    await http.api.get(`${ApiRoutes.AUTH}/status`, {
+      headers: {
+        Cookie: ctx.req.headers.cookie!,
+      },
+    });
+
+    return {
+      redirect: {
+        permanent: false,
+        destination: AppRoutes.DASHBOARD,
+      },
+    };
+  } catch {
+    return {
+      props: {},
+    };
+  }
+};
--- packages/client/src/pages/register/index.tsx
+++ packages/client/src/pages/register/index.tsx
@@ -2,6 +2,9 @@ import SEO from '../../components/SEO';
 import AuthCard from '../../features/Auth/components/Card';
 import AuthLayout from '../../layout/Auth';
 import RegisterForm from '../../features/Auth/components/Form/Register';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { ApiRoutes, AppRoutes } from '../../config/routes';
+import http from '../../config/http';
 
 const Register = () => {
   return (
@@ -17,3 +20,24 @@ const Register = () => {
 };
 
 export default Register;
+
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  try {
+    await http.api.get(`${ApiRoutes.AUTH}/status`, {
+      headers: {
+        Cookie: ctx.req.headers.cookie!,
+      },
+    });
+
+    return {
+      redirect: {
+        permanent: false,
+        destination: AppRoutes.DASHBOARD,
+      },
+    };
+  } catch {
+    return {
+      props: {},
+    };
+  }
+};
