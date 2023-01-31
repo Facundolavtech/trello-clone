ae2a71b659247323a118dbd1bc54cfccfaca32a5
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-30 23:13:53 -0300

    refactor(client): remove title tag in document

--- packages/client/src/pages/_document.tsx
+++ packages/client/src/pages/_document.tsx
@@ -6,7 +6,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Thullo</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
