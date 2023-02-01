b2d8468be6e19c5974c4d67afbd6a9d746b99d93
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-31 23:50:45 -0300

    refactor(client): increased the stale time in query client, added react query hydration

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,35 +1,41 @@
 import '../styles/globals.css';
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import config from '../config';
 import NextProgress from '../libs/NextProgress';
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClientProvider, QueryClient, Hydrate } from '@tanstack/react-query';
 import SEO from '../components/SEO';
 
 function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        staleTime: 30000,
-        retry: false,
-      },
-    },
-  });
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnWindowFocus: false,
+						staleTime: 130000,
+						retry: false,
+					},
+				},
+			})
+	);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <GoogleOAuthProvider clientId={config.Auth.Providers.Google.clientId} onScriptLoadError={() => null}>
-        <ChakraProvider theme={theme}>
-          <NextProgress />
-          <SEO />
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </GoogleOAuthProvider>
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<GoogleOAuthProvider clientId={config.Auth.Providers.Google.clientId} onScriptLoadError={() => null}>
+				<ChakraProvider theme={theme}>
+					<NextProgress />
+					<Hydrate state={pageProps.dehydratedState}>
+						<SEO />
+						<Component {...pageProps} />
+					</Hydrate>
+				</ChakraProvider>
+			</GoogleOAuthProvider>
+		</QueryClientProvider>
+	);
 }
 
 export default App;
