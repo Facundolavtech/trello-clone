a61777ad41e777d031dfac84dee866b920fd2a34
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-31 23:48:59 -0300

    refactor(client): wrap button component in forward ref for better performance

--- packages/client/src/components/Button/index.tsx
+++ packages/client/src/components/Button/index.tsx
@@ -1,60 +1,48 @@
-import { CSSProperties, FC, ReactNode } from 'react';
+import { CSSProperties, FC, ReactNode, forwardRef } from 'react';
 import { Button as ChakraButton, As } from '@chakra-ui/react';
 import { FontFamily } from '../../theme/constants';
 
 type Props = {
-  children: ReactNode;
-  width?: number | string;
-  as?: As;
-  height?: number | string;
-  type?: 'button' | 'submit';
-  px?: number;
-  py?: number;
-  variant: 'primary' | 'lightgray' | 'outline' | 'link';
-  fontFamily?: FontFamily;
-  fontWeight?: number;
-  disabled?: boolean;
-  style?: CSSProperties;
-  loading?: boolean;
-  onClick?: (params: any) => any;
+	children: ReactNode;
+	width?: number | string;
+	as?: As;
+	height?: number | string;
+	type?: 'button' | 'submit';
+	px?: number;
+	py?: number;
+	variant: 'primary' | 'lightgray' | 'outline' | 'link';
+	fontFamily?: FontFamily;
+	fontWeight?: number;
+	disabled?: boolean;
+	style?: CSSProperties;
+	loading?: boolean;
+	onClick?: (params: any) => any;
 };
 
-const Button: FC<Props> = ({
-  children,
-  width,
-  type = 'button',
-  as,
-  px,
-  py,
-  variant,
-  fontWeight = 500,
-  fontFamily,
-  disabled = false,
-  style,
-  onClick,
-  loading = false,
-}) => {
-  return (
-    <ChakraButton
-      width={width}
-      height="auto"
-      as={as}
-      px={px}
-      py={py}
-      fontWeight={fontWeight}
-      type={type}
-      fontFamily={fontFamily}
-      variant={variant}
-      isDisabled={disabled}
-      isLoading={loading}
-      style={style}
-      display="flex"
-      alignItems="center"
-      onClick={onClick}
-    >
-      {children}
-    </ChakraButton>
-  );
-};
+const Button: FC<Props> = forwardRef(
+	({ children, width, type = 'button', as, px, py, variant, fontWeight = 500, fontFamily, disabled = false, style, onClick, loading = false }, ref) => {
+		return (
+			<ChakraButton
+				width={width}
+				height="auto"
+				as={as}
+				px={px}
+				py={py}
+				fontWeight={fontWeight}
+				type={type}
+				fontFamily={fontFamily}
+				variant={variant}
+				isDisabled={disabled}
+				isLoading={loading}
+				style={style}
+				display="flex"
+				alignItems="center"
+				onClick={onClick}
+			>
+				{children}
+			</ChakraButton>
+		);
+	}
+);
 
 export default Button;
