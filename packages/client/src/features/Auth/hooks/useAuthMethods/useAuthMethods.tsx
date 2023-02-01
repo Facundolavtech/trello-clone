0f35d7ee6b125d417bb868b47b49f78d8c909c10
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-31 23:51:29 -0300

    refactor(client): moved auth hooks to their folders

---
+++ packages/client/src/features/Auth/hooks/useAuthMethods/useAuthMethods.tsx
@@ -0,0 +1,83 @@
+import { useToast } from '@chakra-ui/react';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/router';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import http from '../../../../config/http';
+import { ApiRoutes, AppRoutes } from '../../../../config/routes';
+import { ILoginFormValues } from '../../components/Form/Login';
+import { IRegisterFormValues } from '../../components/Form/Register';
+import SocialProviders from '../../constants/providers';
+
+const useAuthMethods = () => {
+	const router = useRouter();
+	const toast = useToast();
+	const queryClient = useQueryClient();
+
+	const loginSocialMutation = useMutation(
+		({ provider, token }: { provider: SocialProviders; token: string }) => http.api.post(`${ApiRoutes.AUTH}/${provider}`, { token }),
+		{
+			onSuccess: () => {
+				return router.push(AppRoutes.DASHBOARD);
+			},
+			onError: (err: AxiosError<any, any>) => {
+				toast({
+					isClosable: false,
+					status: 'error',
+					title: err.response?.data.message,
+					position: 'top-right',
+					variant: 'solid',
+					duration: 1500,
+				});
+			},
+		}
+	);
+
+	const loginLocalMutation = useMutation((credentials: ILoginFormValues) => http.api.post(`${ApiRoutes.AUTH}/local/login`, credentials), {
+		onSuccess: () => {
+			return router.push(AppRoutes.DASHBOARD);
+		},
+		onError: (err: AxiosError<any, any>) => {
+			toast({
+				isClosable: false,
+				status: 'error',
+				title: err.response?.data.message,
+				position: 'top-right',
+				variant: 'solid',
+				duration: 1500,
+			});
+		},
+	});
+
+	const registerLocalMutation = useMutation((credentials: IRegisterFormValues) => http.api.post(`${ApiRoutes.AUTH}/local/register`, credentials), {
+		onSuccess: () => {
+			return router.push(AppRoutes.DASHBOARD);
+		},
+		onError: (err: AxiosError<any, any>) => {
+			toast({
+				isClosable: false,
+				status: 'error',
+				title: err.response?.data.message,
+				position: 'top-right',
+				variant: 'solid',
+				duration: 1500,
+			});
+		},
+	});
+
+	const logoutQuery = useQuery(['auth/logout'], () => http.api.get(`${ApiRoutes.AUTH}/logout`), {
+		enabled: false,
+		onSuccess: () => {
+			queryClient.removeQueries(['user/profile']);
+			router.push(AppRoutes.LOGIN);
+		},
+	});
+
+	return {
+		loginSocialMutation,
+		loginLocalMutation,
+		registerLocalMutation,
+		logout: logoutQuery.refetch,
+	};
+};
+
+export default useAuthMethods;
---
+++ packages/client/src/features/Auth/hooks/useGoogleToken/useGoogleToken.tsx
@@ -0,0 +1,20 @@
+import { useGoogleLogin } from '@react-oauth/google';
+import SocialProviders from '../../constants/providers';
+import useAuthMethods from '../useAuthMethods/useAuthMethods';
+
+const useGoogleToken = () => {
+	const { loginSocialMutation } = useAuthMethods();
+
+	const getTokenAndLogin = useGoogleLogin({
+		onSuccess: (res) => {
+			return loginSocialMutation.mutate({ provider: SocialProviders.GOOGLE, token: res.access_token });
+		},
+		onError: () => {
+			return;
+		},
+	});
+
+	return { getTokenAndLogin };
+};
+
+export default useGoogleToken;
