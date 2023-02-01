1167bf885a5fc47866e4a8de00deec520480985e
Facundo Lavagnino <flavagnino8@gmail.com>
2023-01-31 23:55:00 -0300

    refactor(client): getserversideprops is eliminated in login and register

--- packages/client/src/pages/login/index.tsx
+++ packages/client/src/pages/login/index.tsx
@@ -1,43 +1,20 @@
-import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import SEO from '../../components/SEO';
-import http from '../../config/http';
-import { ApiRoutes, AppRoutes } from '../../config/routes';
 import AuthCard from '../../features/Auth/components/Card';
 import LoginForm from '../../features/Auth/components/Form/Login';
+import { withSession } from '../../hoc/withSession';
 import AuthLayout from '../../layout/Auth';
 
 const Login = () => {
-  return (
-    <>
-      <SEO title="Login" />
-      <AuthLayout>
-        <AuthCard formType="login">
-          <LoginForm />
-        </AuthCard>
-      </AuthLayout>
-    </>
-  );
+	return (
+		<>
+			<SEO title="Login" />
+			<AuthLayout>
+				<AuthCard formType="login">
+					<LoginForm />
+				</AuthCard>
+			</AuthLayout>
+		</>
+	);
 };
 
-export default Login;
-
-export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  try {
-    await http.api.get(`${ApiRoutes.AUTH}/status`, {
-      headers: {
-        Cookie: ctx.req.headers.cookie!,
-      },
-    });
-
-    return {
-      redirect: {
-        permanent: false,
-        destination: AppRoutes.DASHBOARD,
-      },
-    };
-  } catch {
-    return {
-      props: {},
-    };
-  }
-};
+export default withSession(Login);
--- packages/client/src/pages/register/index.tsx
+++ packages/client/src/pages/register/index.tsx
@@ -2,42 +2,19 @@ import SEO from '../../components/SEO';
 import AuthCard from '../../features/Auth/components/Card';
 import AuthLayout from '../../layout/Auth';
 import RegisterForm from '../../features/Auth/components/Form/Register';
-import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import { ApiRoutes, AppRoutes } from '../../config/routes';
-import http from '../../config/http';
+import { withSession } from '../../hoc/withSession';
 
 const Register = () => {
-  return (
-    <>
-      <SEO title="Register" />
-      <AuthLayout>
-        <AuthCard formType="register">
-          <RegisterForm />
-        </AuthCard>
-      </AuthLayout>
-    </>
-  );
+	return (
+		<>
+			<SEO title="Register" />
+			<AuthLayout>
+				<AuthCard formType="register">
+					<RegisterForm />
+				</AuthCard>
+			</AuthLayout>
+		</>
+	);
 };
 
-export default Register;
-
-export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  try {
-    await http.api.get(`${ApiRoutes.AUTH}/status`, {
-      headers: {
-        Cookie: ctx.req.headers.cookie!,
-      },
-    });
-
-    return {
-      redirect: {
-        permanent: false,
-        destination: AppRoutes.DASHBOARD,
-      },
-    };
-  } catch {
-    return {
-      props: {},
-    };
-  }
-};
+export default withSession(Register);
