5509b55c256e6b807a8683ef81c9e9c2a8199b5b
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-01 00:01:36 -0300

    feat(client): add loading skeleton to dashboard menu

--- packages/client/src/components/Dashboard/Menu/Button/index.tsx
+++ packages/client/src/components/Dashboard/Menu/Button/index.tsx
@@ -1,25 +1,25 @@
 import { Avatar, HStack, MenuButton as ChakraMenuButton, Text } from '@chakra-ui/react';
 import { RiArrowDownSFill } from 'react-icons/ri';
-import { useQuery } from 'react-query';
 import { IUser } from '../../../../models/user.model';
 import { FontFamily } from '../../../../theme/constants';
+import useQueryData from '../../../../hooks/useQueryData';
 
 const MenuButton = () => {
-  const { data } = useQuery<IUser>('user/profile');
+	const data: IUser = useQueryData('user/profile');
 
-  return (
-    <ChakraMenuButton transition="all 0.2s">
-      <HStack spacing="20px" alignItems="center">
-        <HStack spacing="11px">
-          <Avatar width="32px" height="32px" name={data?.name} src={data?.picture || ''} bg={data?.picture ? 'transparent' : 'gray.4'} color="gray.1" />
-          <Text color="gray.1" fontSize={12} fontWeight={700} fontFamily={FontFamily.NotoSans}>
-            {data?.name}
-          </Text>
-        </HStack>
-        <RiArrowDownSFill color="gray.1" />
-      </HStack>
-    </ChakraMenuButton>
-  );
+	return (
+		<ChakraMenuButton transition="all 0.2s">
+			<HStack spacing="20px" alignItems="center">
+				<HStack spacing="11px">
+					<Avatar width="32px" height="32px" name={data.name} src={data.picture || ''} bg={data.picture ? 'transparent' : 'gray.4'} color="gray.1" />
+					<Text color="gray.1" fontSize={12} fontWeight={700} fontFamily={FontFamily.NotoSans}>
+						{data.name}
+					</Text>
+				</HStack>
+				<RiArrowDownSFill color="gray.1" />
+			</HStack>
+		</ChakraMenuButton>
+	);
 };
 
 export default MenuButton;
--- packages/client/src/components/Dashboard/Menu/index.tsx
+++ packages/client/src/components/Dashboard/Menu/index.tsx
@@ -1,38 +1,45 @@
-import { Divider, Menu as ChakraMenu, MenuList, Text, VStack } from '@chakra-ui/react';
+import { Divider, HStack, Menu as ChakraMenu, MenuList, SkeletonCircle, SkeletonText, Text, VStack } from '@chakra-ui/react';
 import MenuItems from './Items';
 import MenuButton from './Button';
-import { useQuery } from 'react-query';
-import { IUser } from '../../../models/user.model';
 import formatDate from 'date-fns/format';
 import { fromUnixTime } from 'date-fns';
+import { IUser } from '../../../models/user.model';
+import useQueryState from '../../../hooks/useQueryState';
 
 const Menu = () => {
-  const { data } = useQuery<IUser>('user/profile');
+	const state = useQueryState<IUser>('user/profile');
 
-  if (!data) return null;
+	if (state.status === 'loading') {
+		return (
+			<HStack spacing={4}>
+				<SkeletonCircle size="8" />
+				<SkeletonText noOfLines={2}>Loading user</SkeletonText>
+			</HStack>
+		);
+	}
 
-  return (
-    <ChakraMenu>
-      <MenuButton />
-      <MenuList padding={4}>
-        <VStack alignItems="flex-start">
-          <Text color="gray.4" fontSize={14} fontWeight={400}>
-            {data.email}
-          </Text>
-          <Text color="gray.4" fontSize={14} fontWeight={400}>
-            Register at:{' '}
-            <Text as="strong" color="gray.3" fontWeight={500}>
-              {formatDate(fromUnixTime(data.createdAt), 'dd/MM/yyyy')}
-            </Text>
-          </Text>
-        </VStack>
-        <Divider orientation="horizontal" width="full" borderColor="rgba(0,0,0,0.1)" my={4} />
-        <VStack>
-          <MenuItems />
-        </VStack>
-      </MenuList>
-    </ChakraMenu>
-  );
+	return (
+		<ChakraMenu>
+			<MenuButton />
+			<MenuList padding={4}>
+				<VStack alignItems="flex-start">
+					<Text color="gray.4" fontSize={14} fontWeight={400}>
+						{state.data?.email}
+					</Text>
+					<Text color="gray.4" fontSize={14} fontWeight={400}>
+						Register at:{' '}
+						<Text as="strong" color="gray.3" fontWeight={500}>
+							{formatDate(fromUnixTime(state.data?.createdAt || 0), 'dd/MM/yyyy')}
+						</Text>
+					</Text>
+				</VStack>
+				<Divider orientation="horizontal" width="full" borderColor="rgba(0,0,0,0.1)" my={4} />
+				<VStack>
+					<MenuItems />
+				</VStack>
+			</MenuList>
+		</ChakraMenu>
+	);
 };
 
 export default Menu;
