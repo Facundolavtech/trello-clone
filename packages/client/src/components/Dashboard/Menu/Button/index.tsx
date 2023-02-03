825d1d8a2fa8c5ddc85878e5934edb78101e8caf
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-02 21:25:47 -0300

    refactor(client): use user profile custom hook in dashboard menu button

--- packages/client/src/components/Dashboard/Menu/Button/index.tsx
+++ packages/client/src/components/Dashboard/Menu/Button/index.tsx
@@ -1,19 +1,18 @@
 import { Avatar, HStack, MenuButton as ChakraMenuButton, Text } from '@chakra-ui/react';
 import { RiArrowDownSFill } from 'react-icons/ri';
-import { IUser } from '../../../../models/user.model';
 import { FontFamily } from '../../../../theme/constants';
-import useQueryData from '../../../../hooks/useQueryData';
+import useUserProfile from '../../../../hooks/useUserProfile';
 
 const MenuButton = () => {
-  const data: IUser = useQueryData('user/profile');
+  const { data: user } = useUserProfile();
 
   return (
     <ChakraMenuButton transition="all 0.2s">
       <HStack spacing="20px" alignItems="center">
         <HStack spacing="11px">
-          <Avatar width="32px" height="32px" name={data?.name} src={data?.picture || ''} bg={data?.picture ? 'transparent' : 'gray.4'} color="gray.1" />
+          <Avatar width="32px" height="32px" name={user?.name} src={user?.picture || ''} bg={user?.picture ? 'transparent' : 'gray.4'} color="gray.1" />
           <Text color="gray.1" fontSize={12} fontWeight={700} fontFamily={FontFamily.NotoSans}>
-            {data?.name}
+            {user?.name}
           </Text>
         </HStack>
         <RiArrowDownSFill color="gray.1" />
