d6c750485b1aaa2b320740062fd531861e06c7b3
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-02 21:18:03 -0300

    feat(client): add wrapped container to board layout

--- packages/client/src/features/Board/layouts/BoardLayout/index.tsx
+++ packages/client/src/features/Board/layouts/BoardLayout/index.tsx
@@ -1,4 +1,6 @@
+import { Stack } from '@chakra-ui/react';
 import { FC, ReactNode } from 'react';
+import WrappedContainer from '../../../../components/Containers/Wrapped';
 import SEO from '../../../../components/SEO';
 import DashboardLayout from '../../../../layout/Dashboard';
 
@@ -11,7 +13,13 @@ const BoardLayout: FC<Props> = ({ title, children }) => {
   return (
     <>
       <SEO title={title} />
-      <DashboardLayout>{children}</DashboardLayout>
+      <DashboardLayout>
+        <WrappedContainer>
+          <Stack width="full" mt="35px">
+            {children}
+          </Stack>
+        </WrappedContainer>
+      </DashboardLayout>
     </>
   );
 };
