13283b5e7633de41bc76fa2550cbe49aa53aa6aa
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-03 23:01:51 -0300

    feat(client): add fonts locally

--- packages/client/src/pages/_document.tsx
+++ packages/client/src/pages/_document.tsx
@@ -7,8 +7,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="referrer" content="no-referrer" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <ColorModeScript initialColorMode={themeConfig.initialColorMode} />
