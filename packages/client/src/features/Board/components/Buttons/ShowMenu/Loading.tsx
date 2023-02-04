79f473f1508ffd10afd52f546deb87729cf4b721
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-03 21:24:37 -0300

    feat(client): add loader in board menu button

---
+++ packages/client/src/features/Board/components/Buttons/ShowMenu/Loading.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from '@chakra-ui/react';
+
+const Loading = () => {
+  return <Skeleton width="117px" height="32px" borderRadius="8px" />;
+};
+
+export default Loading;
--- packages/client/src/features/Board/components/Buttons/ShowMenu/index.tsx
+++ packages/client/src/features/Board/components/Buttons/ShowMenu/index.tsx
@@ -1,8 +1,21 @@
 import { Icon, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import { MdMoreHoriz } from 'react-icons/md';
 import Button from '../../../../../components/Button';
+import useBoard from '../../../hooks/useBoard';
+import Loading from './Loading';
 
 const ShowBoardMenuButton = () => {
+  const router = useRouter();
+
+  const boardId = router.query.id as string;
+
+  const { isLoading } = useBoard({ id: boardId });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <Button variant="lightgray" width="117px" height="32px" style={{ gap: '12px' }}>
       <Icon as={MdMoreHoriz} fontSize={13} color="gray.3" />
