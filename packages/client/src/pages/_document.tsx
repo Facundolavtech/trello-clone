71497be876ffab16cd644cd85e33f7e1c8e0ba67
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-03 22:38:49 -0300

    refactor(client): remove link tags in document

--- packages/client/src/pages/_document.tsx
+++ packages/client/src/pages/_document.tsx
@@ -7,8 +7,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="referrer" content="no-referrer" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;500;600;700&display=swap" rel="stylesheet" />
       </Head>
