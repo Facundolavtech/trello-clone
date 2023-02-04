9f8c962b7864441188c1cec05dbca91d2ebe1de1
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-04 00:24:10 -0300

    refactor(server): change dto messages to more declarative messages, add color palette validation

--- packages/server/src/modules/Board/modules/Card/modules/Label/dto/label.dto.ts
+++ packages/server/src/modules/Board/modules/Card/modules/Label/dto/label.dto.ts
@@ -1,23 +1,31 @@
-import { IsHexColor, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsHexColor, IsIn, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
+const availableColors = ['#219653', '#F2C94C', '#F2994A', '#EB5757', '#2F80ED', '#56CCF2', '#6FCF97', '#333333', '#4F4F4F', '#828282', '#BDBDBD', '#E0E0E0'];
 
 export class CreateCardLabelDTO {
-  @IsNotEmpty({ message: 'Error: The field is required' })
-  @IsString({ message: 'Error: The field value is not valid' })
-  @MaxLength(12, { message: 'Error: The name must contain a maximum of 12 characters' })
+  @IsNotEmpty({ message: 'Error: Card label name required' })
+  @IsString({ message: 'Error: Card label name is not valid' })
+  @MaxLength(12, { message: 'Error: Card label name must contain a maximum of 12 characters' })
   name: string;
 
-  @IsNotEmpty({ message: 'Error: The field is required' })
-  @IsHexColor({ message: 'Error: The field value is not valid' })
+  @IsNotEmpty({ message: 'Error: Card label color is required' })
+  @IsHexColor({ message: 'Error: Card label color is not valid' })
+  @IsIn(availableColors, {
+    message: 'Error: Card label color is not valid',
+  })
   color: string;
 }
 
 export class UpdateCardLabelDTO {
   @IsOptional()
-  @IsString({ message: 'Error: The field value is not valid' })
-  @MaxLength(12, { message: 'Error: The name must contain a maximum of 12 characters' })
+  @IsString({ message: 'Error: Card label name is not valid' })
+  @MaxLength(12, { message: 'Error: Card label name must contain a maximum of 12 characters' })
   name?: string;
 
   @IsOptional()
-  @IsHexColor({ message: 'Error: The field value is not valid' })
+  @IsHexColor({ message: 'Error: Card label color is not valid' })
+  @IsIn(availableColors, {
+    message: 'Error: Card label color is not valid',
+  })
   color?: string;
 }
