93556bf4fbb67e75a3f96894bca7fc666d3df591
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-04 23:01:36 -0300

    feat(client): add button to create new board list after last list

--- packages/client/src/features/Board/components/BoardPage/index.tsx
+++ packages/client/src/features/Board/components/BoardPage/index.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
-import { HStack, Stack, VStack } from '@chakra-ui/react';
+import { Flex, HStack, Icon, Stack, Text, VStack } from '@chakra-ui/react';
 import useBoard from '../../hooks/useBoard';
 import BoardLayout from '../../layouts/BoardLayout';
 import BoardMembers from '../Members';
-import PrivacyBoardButton from '../Buttons/Privacy';
+import BoardPrivacy from '../BoardPrivacy';
 import ShowBoardMenuButton from '../Buttons/ShowMenu';
 import Lists from '../../../BoardList/components/Lists';
 import Error from './Error';
+import Button from '../../../../components/Button';
+import { FontFamily } from '../../../../theme/constants';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 type Props = {
   id: string;
@@ -29,14 +32,20 @@ const BoardPage: FC<Props> = ({ id }) => {
         <Stack width="full" direction={{ base: 'column', md: 'row' }} pb={10}>
           <VStack width="full" alignItems="flex-start" spacing="52px">
             <HStack width="full" justifyContent="space-between">
-              <HStack spacing="19px" width="full">
-                <PrivacyBoardButton />
+              <Flex gap="19px" width="full">
+                <BoardPrivacy />
                 <BoardMembers />
-              </HStack>
+              </Flex>
               <ShowBoardMenuButton />
             </HStack>
             <HStack width="full" spacing="35px" overflowX="auto" alignItems="flex-start">
               <Lists />
+              <Button width="244px" height="32px" variant="primary" style={{ background: '#DAE4FD', justifyContent: 'space-between' }}>
+                <Text color="blue.1" fontWeight={500} fontSize={12} fontFamily={FontFamily.NotoSans}>
+                  Add another list
+                </Text>
+                <Icon as={AiOutlinePlus} color="blue.1" fontSize={12} />
+              </Button>
             </HStack>
           </VStack>
         </Stack>
