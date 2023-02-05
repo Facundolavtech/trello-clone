e90eec71e80bdc142f23dd8cf41413d08673430c
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-04 22:58:33 -0300

    feat(client): add handle board privacy service

--- packages/client/src/features/Board/services/board.service.ts
+++ packages/client/src/features/Board/services/board.service.ts
@@ -3,6 +3,13 @@ import http from '../../../config/http';
 import { ApiRoutes } from '../../../config/routes';
 import { IBoard } from '../../../models/board.model';
 
+export type BoardPrivacyType = 'private' | 'public';
+
+export interface IHandleBoardPrivacyParams {
+  type: BoardPrivacyType;
+  id: string;
+}
+
 export async function getBoardById(id: string): Promise<IBoard> {
   const response: AxiosResponse<IBoard> = await http.api.get(`${ApiRoutes.BOARD}/${id}`);
   return response.data;
@@ -10,5 +17,12 @@ export async function getBoardById(id: string): Promise<IBoard> {
 
 export async function getBoards() {
   const response: AxiosResponse<IBoard[]> = await http.api.get(`${ApiRoutes.BOARD}`);
+  return response.data;
+}
+
+export async function handleBoardPrivacy({ type, id }: IHandleBoardPrivacyParams): Promise<IBoard> {
+  const formattedPrivacyType = type === 'private' ? true : false;
+
+  const response: AxiosResponse<IBoard> = await http.api.put(`${ApiRoutes.BOARD}/update/${id}`, { isPrivate: formattedPrivacyType });
   return response.data;
 }
