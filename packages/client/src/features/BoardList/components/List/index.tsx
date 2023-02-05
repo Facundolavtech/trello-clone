85a23a313a4bb4864fdd27925af7492b60d1436e
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-04 22:56:43 -0300

    feat(client): add button to create new card after last card in every board list

--- packages/client/src/features/BoardList/components/List/index.tsx
+++ packages/client/src/features/BoardList/components/List/index.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react';
-import { Icon, Heading, HStack, Box, VStack } from '@chakra-ui/react';
+import { Icon, Heading, HStack, Box, VStack, Text } from '@chakra-ui/react';
 import { MdMoreHoriz } from 'react-icons/md';
 import { BoardList } from '../../../../models/board-list.model';
 import Cards from '../../../BoardCard/components/Cards';
+import Button from '../../../../components/Button';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { FontFamily } from '../../../../theme/constants';
 
 type Props = {
   list: BoardList;
@@ -12,7 +15,7 @@ const List: FC<Props> = ({ list }) => {
   if (!list) return null;
 
   return (
-    <VStack>
+    <VStack spacing="17px">
       <HStack width={{ base: 'full', md: '240px' }} justifyContent="space-between" alignItems="center">
         <Heading color="gray.1" fontWeight={500} fontSize={14}>
           {list.name}
@@ -22,6 +25,12 @@ const List: FC<Props> = ({ list }) => {
         </Box>
       </HStack>
       <Cards cards={list.cards} />
+      <Button width="full" height="32px" variant="primary" style={{ background: '#DAE4FD', justifyContent: 'space-between' }}>
+        <Text color="blue.1" fontWeight={500} fontSize={12} fontFamily={FontFamily.NotoSans}>
+          Add another card
+        </Text>
+        <Icon as={AiOutlinePlus} color="blue.1" fontSize={12} />
+      </Button>
     </VStack>
   );
 };
