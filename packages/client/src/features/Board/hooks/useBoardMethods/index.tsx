d12aaa8945db870c6f094ded4d3e1bbb2d05fd98
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-04 22:58:17 -0300

    feat(client): add handle board privacy mutation

---
+++ packages/client/src/features/Board/hooks/useBoardMethods/index.tsx
@@ -0,0 +1,41 @@
+import { useToast } from '@chakra-ui/react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { IBoard } from '../../../../models/board.model';
+import { BoardPrivacyType, handleBoardPrivacy } from '../../services/board.service';
+
+type Props = {
+  id: string;
+};
+
+const useBoardMethods = ({ id }: Props) => {
+  const queryClient = useQueryClient();
+  const toast = useToast();
+
+  const handleBoardPrivacyMutation = useMutation(({ type }: { type: BoardPrivacyType }) => handleBoardPrivacy({ type, id }), {
+    onSuccess: (data: IBoard) => {
+      queryClient.setQueryData([`board/${id}`], (oldData) =>
+        oldData
+          ? {
+              ...oldData,
+              isPrivate: data.isPrivate,
+            }
+          : oldData
+      );
+    },
+    onError: (err: AxiosError<any>) => {
+      return toast({
+        position: 'top-right',
+        duration: 2000,
+        isClosable: false,
+        status: 'error',
+        title: 'Error',
+        description: err.response?.data.message || 'An error occurred while trying to update the board privacy',
+      });
+    },
+  });
+
+  return { handleBoardPrivacyMutation };
+};
+
+export default useBoardMethods;
