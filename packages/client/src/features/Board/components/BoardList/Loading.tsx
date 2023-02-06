1062877944980739eae8f5381c3e646765d4f132
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-06 02:38:40 -0300

    refactor(client): change board list loader

--- packages/client/src/features/Board/components/BoardList/Loading.tsx
+++ packages/client/src/features/Board/components/BoardList/Loading.tsx
@@ -1,14 +1,22 @@
-import { SimpleGrid, Skeleton } from '@chakra-ui/react';
-import Card from '../../../../components/Card';
+import { HStack, SimpleGrid, Skeleton, VStack } from '@chakra-ui/react';
+import SkeletonText from '../../../../components/SkeletonText';
 
 const Loading = () => {
   return (
     <SimpleGrid width="full" gap={8} columns={{ base: 1, md: 3, lg: 4 }}>
-      {new Array(4).fill(4).map((_, index) => {
+      {new Array(4).fill(4).map((_, i) => {
         return (
-          <Card key={index} variant="board-card" height="230px">
-            <Skeleton width="full" height="full" />
-          </Card>
+          <VStack spacing="21px" key={i}>
+            <VStack width="full" spacing="12px" alignItems="flex-start">
+              <Skeleton width="full" height="130px" borderRadius="8px" />
+              <SkeletonText containerWidth="50%" />
+            </VStack>
+            <HStack spacing="12px" width="full">
+              {new Array(3).fill(3).map((_, i) => {
+                return <Skeleton width="28px" height="28px" borderRadius="8px" key={i} />;
+              })}
+            </HStack>
+          </VStack>
         );
       })}
     </SimpleGrid>
