f11d4eb781aaf8d46dac9ab26884a8a43ad0f92a
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-05 23:57:52 -0300

    feat(client): add new board values types and handle close modal function

--- packages/client/src/features/Board/components/Modals/NewBoard/index.tsx
+++ packages/client/src/features/Board/components/Modals/NewBoard/index.tsx
@@ -1,35 +1,36 @@
 import { useState } from 'react';
-import {
-  Image,
-  Menu as ChakraMenu,
-  MenuButton as ChakraMenuButton,
-  Modal,
-  ModalContent,
-  ModalOverlay,
-  Input,
-  HStack,
-  Icon,
-  Text,
-  Flex,
-  Wrap,
-  MenuList,
-  WrapItem,
-} from '@chakra-ui/react';
+import { Modal, ModalContent, ModalOverlay, HStack, Flex } from '@chakra-ui/react';
 import NiceModal, { useModal } from '@ebay/nice-modal-react';
-import { AiFillPicture, AiOutlinePlus } from 'react-icons/ai';
-import { MdOutlineClose } from 'react-icons/md';
-import Button from '../../../../../components/Button';
 import boardCovers from '../../../helpers/covers';
-import { IoMdLock, IoMdUnlock } from 'react-icons/io';
+import Title from './TitleInput';
+import CoverMenu from './CoverMenu';
+import SubmitButton from './Buttons/Submit';
+import CancelButton from './Buttons/Cancel';
+import CloseModalButton from './Buttons/Close';
+import Cover from './Cover';
+import PrivacyButton from './Buttons/Privacy';
+
+export interface INewBoardValues {
+  title: string;
+  cover: INewBoardValuesCover;
+  isPrivate: boolean;
+}
+
+interface INewBoardValuesCover {
+  name: string;
+  src: string;
+}
+
+const initialNewBoardValues: INewBoardValues = {
+  cover: boardCovers[Math.floor(Math.random() * boardCovers.length)],
+  title: '',
+  isPrivate: true,
+};
 
 const NewBoardModal = NiceModal.create(() => {
   const modal = useModal();
 
-  const [newBoardValues, setNewBoardValues] = useState({
-    cover: boardCovers[Math.floor(Math.random() * boardCovers.length)],
-    title: '',
-    isPrivate: true,
-  });
+  const [newBoardValues, setNewBoardValues] = useState(initialNewBoardValues);
 
   const handleSubmit = () => {
     if (!newBoardValues.title.trim() || !newBoardValues.cover.src) return;
@@ -37,112 +38,25 @@ const NewBoardModal = NiceModal.create(() => {
     console.log(newBoardValues);
   };
 
+  const handleHideModal = () => {
+    setNewBoardValues(initialNewBoardValues);
+    modal.hide();
+  };
+
   return (
-    <Modal isOpen={modal.visible} onClose={() => modal.remove()}>
+    <Modal isOpen={modal.visible} onClose={handleHideModal}>
       <ModalOverlay />
       <ModalContent py="27px" px="24px" borderRadius="8px" maxW="90%" width="307px" position="relative">
-        <Button variant="primary" width="32px" height="32px" position="absolute" right="11px" top="11px" onClick={() => modal.remove()}>
-          <Icon as={MdOutlineClose} color="white" fontSize={22} />
-        </Button>
-        <Image mb="10px" width="full" height="78px" borderRadius="8px" src={newBoardValues.cover.src} alt="Board cover preview" objectFit="cover" />
-        <Input
-          type="text"
-          width="full"
-          height="34px"
-          borderWidth={1}
-          borderColor="#E0E0E0"
-          borderRadius="8px"
-          onChange={(e) => setNewBoardValues({ ...newBoardValues, title: e.target.value })}
-          _placeholder={{ color: 'gray.4', fontWeight: 500, fontSize: 10 }}
-          filter="drop-shadow(0px 4px 12px rgba(0, 0, 0, 0.1))"
-          fontSize={10}
-          py="10px"
-          px="15px"
-          mb="21px"
-          placeholder="Add board title"
-        />
+        <CloseModalButton onClick={handleHideModal} />
+        <Cover src={newBoardValues.cover.src} />
+        <Title onChange={(e) => setNewBoardValues({ ...newBoardValues, title: e.target.value })} />
         <Flex width="full" justifyContent="space-between" mb="22px">
-          <ChakraMenu>
-            {({ onClose }) => (
-              <>
-                <ChakraMenuButton>
-                  <Button as="div" width="120px" height="32px" variant="lightgray" style={{ gap: 12, justifyContent: 'flex-start' }}>
-                    <Icon as={AiFillPicture} fontSize={14} color="gray.3" />
-                    <Text color="gray.3" fontWeight={500} fontSize={12}>
-                      Cover
-                    </Text>
-                  </Button>
-                </ChakraMenuButton>
-                <MenuList
-                  padding="12px"
-                  width="234px"
-                  height="240px"
-                  maxWidth="full"
-                  borderWidth={1}
-                  borderColor="#E0E0E0"
-                  borderRadius="12px"
-                  boxShadow="0px 2px 4px rgba(0, 0, 0, 0.05)"
-                >
-                  <Wrap
-                    spacing={4}
-                    width="full"
-                    height="full"
-                    overflowY="auto"
-                    css={{
-                      '&::-webkit-scrollbar': {
-                        width: 0,
-                      },
-                      '&::-webkit-scrollbar-track': {
-                        width: 0,
-                      },
-                    }}
-                  >
-                    {boardCovers.map((cover) => (
-                      <WrapItem
-                        _hover={{ cursor: 'pointer' }}
-                        key={cover.name}
-                        onClick={() => {
-                          setNewBoardValues({ ...newBoardValues, cover }), onClose();
-                        }}
-                        width="100%"
-                        height="48px"
-                        borderRadius="8px"
-                        backgroundImage={cover.src}
-                        backgroundPosition="center"
-                        backgroundSize="cover"
-                        backgroundRepeat="no-repeat"
-                      />
-                    ))}
-                  </Wrap>
-                </MenuList>
-              </>
-            )}
-          </ChakraMenu>
-          <Button
-            width="120px"
-            height="32px"
-            variant="lightgray"
-            style={{ gap: 12, justifyContent: 'flex-start' }}
-            onClick={() => setNewBoardValues({ ...newBoardValues, isPrivate: !newBoardValues.isPrivate })}
-          >
-            <Icon as={newBoardValues.isPrivate ? IoMdLock : IoMdUnlock} fontSize={14} color="gray.3" />
-            <Text color="gray.3" fontWeight={500} fontSize={12}>
-              {newBoardValues.isPrivate ? 'Private' : 'Public'}
-            </Text>
-          </Button>
+          <CoverMenu newBoardValues={newBoardValues} setNewBoardValues={setNewBoardValues} />
+          <PrivacyButton newBoardValues={newBoardValues} setNewBoardValues={setNewBoardValues} />
         </Flex>
         <HStack width="full" justifyContent="flex-end" spacing="17px">
-          <Button variant="link" onClick={() => modal.remove()}>
-            <Text fontSize={10} fontWeight={500} color="gray.3">
-              Cancel
-            </Text>
-          </Button>
-          <Button height="30px" width="76px" variant="primary" style={{ gap: 11 }} onClick={handleSubmit}>
-            <Icon as={AiOutlinePlus} fontSize={14} color="white" />
-            <Text color="white" fontWeight={500} fontSize={10}>
-              Create
-            </Text>
-          </Button>
+          <CancelButton onClick={handleHideModal} />
+          <SubmitButton onClick={handleSubmit} />
         </HStack>
       </ModalContent>
     </Modal>
