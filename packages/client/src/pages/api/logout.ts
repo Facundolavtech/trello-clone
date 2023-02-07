471e05bd9be75deae1220b7fbdf2bbb9a06d9a90
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-07 05:49:10 -0300

    refactor(client): change remove cookie method in logout api route

--- packages/client/src/pages/api/logout.ts
+++ packages/client/src/pages/api/logout.ts
@@ -1,6 +1,12 @@
+import { setCookies } from 'cookies-next';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Set-Cookie', 'thullo:sid=; Max-Age=0; Path=/;');
+  setCookies('thullo:sid', '', {
+    req,
+    res,
+    maxAge: 0,
+    path: '/',
+  });
   return res.status(200).json({ success: 'Logged out' });
 };
