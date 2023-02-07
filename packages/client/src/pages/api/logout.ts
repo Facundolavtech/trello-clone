5f8c077f42d06692f3659be9e68e0447383b7f18
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-07 05:44:24 -0300

    fix(client): logout set session cookie max age to 0

--- packages/client/src/pages/api/logout.ts
+++ packages/client/src/pages/api/logout.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Set-Cookie', 'thullo:sid=; Max-Age=-1; Path=/;');
+  res.setHeader('Set-Cookie', 'thullo:sid=; Max-Age=0; Path=/;');
   return res.status(200).json({ success: 'Logged out' });
 };
