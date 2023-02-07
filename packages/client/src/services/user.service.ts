0608d8fe5a62c1df147be2fa42483e908a9fb71e
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-07 01:18:39 -0300

    style(client): add types to get user profile service

--- packages/client/src/services/user.service.ts
+++ packages/client/src/services/user.service.ts
@@ -1,9 +1,10 @@
+import { AxiosResponse } from 'axios';
 import http from '../config/http';
 import { ApiRoutes } from '../config/routes';
 import { IUser } from '../models/user.model';
 
 export async function getUserProfile(): Promise<IUser> {
-  const response = await http.api.get(`${ApiRoutes.USER}/profile`);
+  const response: AxiosResponse<IUser> = await http.api.get(`${ApiRoutes.USER}/profile`);
 
   return response.data;
 }
