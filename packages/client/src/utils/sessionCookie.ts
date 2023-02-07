117252905fae9009db089a8ce777e016a9e89137
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-07 05:33:06 -0300

    refactor(client): remove delete session cookie function

--- packages/client/src/utils/sessionCookie.ts
+++ packages/client/src/utils/sessionCookie.ts
@@ -1,10 +1,6 @@
-import { deleteCookie, setCookie } from 'cookies-next';
+import { setCookie } from 'cookies-next';
 import config from '../config';
 
 export function setSessionCookie(token: string, req?: any, res?: any): void {
   return setCookie('thullo:sid', token, { req, res, ...config.Auth.Cookie });
-}
-
-export function deleteSessionCookie(req?: any, res?: any): void {
-  return deleteCookie('thullo:sid', { req, res, path: config.Auth.Cookie.path });
 }
