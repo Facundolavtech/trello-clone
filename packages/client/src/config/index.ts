60bb334f5be0a795b9f5897094ca4782f6e72198
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-07 02:46:08 -0300

    feat(client): add auth cookie options to global config

--- packages/client/src/config/index.ts
+++ packages/client/src/config/index.ts
@@ -1,3 +1,5 @@
+import { CookieSerializeOptions } from 'cookie';
+
 export default {
   Api: {
     BaseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
@@ -8,5 +10,12 @@ export default {
         clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
       },
     },
+    Cookie: {
+      httpOnly: true,
+      secure: true,
+      maxAge: 7 * 24 * 3600 * 1000,
+      path: '/',
+      sameSite: 'none',
+    } as CookieSerializeOptions,
   },
 };
