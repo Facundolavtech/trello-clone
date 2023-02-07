b3360807129e0e8cca5877663cce22e23cd6efac
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-07 01:36:49 -0300

    feat(client): add axios interceptor to intercept token expired 401 error and redirect to auth page

--- packages/client/src/config/http.ts
+++ packages/client/src/config/http.ts
@@ -1,6 +1,49 @@
-import axios from 'axios';
 import config from '.';
+import axios from 'axios';
+import { createStandaloneToast } from '@chakra-ui/react';
+import Router from 'next/router';
+import { AppRoutes } from './routes';
 
-export default {
-  api: axios.create({ baseURL: config.Api.BaseURL, withCredentials: true }),
+const { toast } = createStandaloneToast();
+
+const http = {
+  api: axios.create({
+    baseURL: config.Api.BaseURL,
+    withCredentials: true,
+  }),
 };
+
+export default http;
+
+let isHandlingError = false;
+
+http.api.interceptors.response.use(
+  (response) => {
+    if (isHandlingError) isHandlingError = false;
+
+    return response;
+  },
+  async (error) => {
+    if (isHandlingError) return Promise.reject(error);
+    isHandlingError = true;
+
+    if (error.response?.status === 401 && error.response?.data.code === 'TokenExpiredError') {
+      try {
+        await axios.get('/api/logout');
+      } catch (error) {
+      } finally {
+        toast({
+          position: 'top-right',
+          title: 'Error',
+          description: error.response.data.message,
+          status: 'error',
+          duration: 2000,
+          isClosable: false,
+        });
+        Router.push(AppRoutes.LOGIN);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
