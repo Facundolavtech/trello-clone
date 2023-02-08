8a59ccadc8fd5f422e0e1058353da3b930fff58f
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-08 18:31:09 -0300

    feat(server): add board title length validation in board dtos

--- packages/server/src/modules/Board/dto/create.dto.ts
+++ packages/server/src/modules/Board/dto/create.dto.ts
@@ -1,17 +1,14 @@
-import { IsBoolean, IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, Matches, Length } from 'class-validator';
 
 export class CreateBoardDTO {
   @IsNotEmpty({ message: 'Error: Board title is required' })
   @IsString({ message: 'Error: Board title is not valid' })
+  @Length(6, 32, { message: 'Error: Board title must have between 6 and 32 characters' })
   title: string;
 
   @IsNotEmpty({ message: 'Error: Board privacy is required' })
   @IsBoolean({ message: 'Error: Board privacy is not valid' })
   isPrivate: boolean;
-
-  @IsNotEmpty({ message: 'Error: Board description is required' })
-  @IsString({ message: 'Error: Board description is not valid' })
-  description: string;
 
   @IsOptional()
   @Matches(/^https:\/\/images\.unsplash\.com\//, { message: 'Board cover needs to be a valid Unsplash URL' })
--- packages/server/src/modules/Board/dto/update.dto.ts
+++ packages/server/src/modules/Board/dto/update.dto.ts
@@ -1,8 +1,9 @@
-import { IsBoolean, IsNotEmpty, IsOptional, IsString, Matches, IsUUID } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, Matches, IsUUID, Length } from 'class-validator';
 
 export class UpdateBoardDTO {
   @IsOptional()
   @IsString({ message: 'Error: Board title is not valid' })
+  @Length(6, 32, { message: 'Error: Board title must have between 6 and 32 characters' })
   title?: string;
 
   @IsOptional()
