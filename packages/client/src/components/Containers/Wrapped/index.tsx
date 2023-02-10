fac6c1d66c3bc7fab7f1d2faf04064d604cd0fd2
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-10 20:17:27 -0300

    feat(client): add rest props to wrapped container

--- packages/client/src/components/Containers/Wrapped/index.tsx
+++ packages/client/src/components/Containers/Wrapped/index.tsx
@@ -4,11 +4,12 @@ import { Box } from '@chakra-ui/react';
 type Props = {
   children: ReactNode;
   styles?: CSSProperties;
+  [x: string]: any;
 };
 
-const WrappedContainer: FC<Props> = ({ children, styles }) => {
+const WrappedContainer: FC<Props> = ({ children, styles, rest }) => {
   return (
-    <Box style={styles} width="90%" maxWidth="1280px" margin="auto">
+    <Box style={styles} width="90%" maxWidth="1280px" margin="auto" {...rest}>
       {children}
     </Box>
   );
