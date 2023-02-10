443da60f8e615c815131a5d05e279237d27335d0
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-10 20:08:07 -0300

    feat(client): add board cover to body image in board page

--- packages/client/src/pages/dashboard/board/[id].tsx
+++ packages/client/src/pages/dashboard/board/[id].tsx
@@ -1,14 +1,32 @@
 import { GetServerSidePropsContext, NextPage } from 'next';
 import BoardPage from '../../../features/Board/components/BoardPage';
 import BoardContextWrapper from '../../../features/Board/context/board';
+import useBoard from '../../../features/Board/hooks/useBoard';
 
 type Props = {
   id: string;
 };
 
+declare module 'react' {
+  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    jsx?: boolean;
+    global?: boolean;
+  }
+}
+
 const Board: NextPage<Props> = ({ id }) => {
+  const { data: board } = useBoard({ id });
+
   return (
     <BoardContextWrapper>
+      <style jsx global>{`
+        body {
+          background-image: url(${board && board.cover});
+          background-repeat: no-repeat;
+          background-size: cover;
+          background-attachment: fixed;
+        }
+      `}</style>
       <BoardPage id={id} />
     </BoardContextWrapper>
   );
