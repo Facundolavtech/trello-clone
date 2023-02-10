ef806629f28476618daa6185386fbc68b6c1d220
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-10 20:10:13 -0300

    refactor(client): add scroll-y on every board list

--- packages/client/src/features/BoardList/components/List/index.tsx
+++ packages/client/src/features/BoardList/components/List/index.tsx
@@ -15,22 +15,46 @@ const List: FC<Props> = ({ list }) => {
   if (!list) return null;
 
   return (
-    <VStack spacing="17px">
-      <HStack width="240px" justifyContent="space-between" alignItems="center">
-        <Heading color="gray.1" fontWeight={500} fontSize={14}>
-          {list.name}
-        </Heading>
-        <Box as="button" display="flex" alignItems="center">
-          <Icon as={MdMoreHoriz} fontSize={16} color="gray.3" />
-        </Box>
-      </HStack>
-      <Cards cards={list.cards} />
-      <Button width="full" height="32px" variant="primary" style={{ background: '#DAE4FD', justifyContent: 'space-between' }}>
-        <Text color="blue.1" fontWeight={500} fontSize={12} fontFamily={FontFamily.NotoSans}>
-          Add another card
-        </Text>
-        <Icon as={AiOutlinePlus} color="blue.1" fontSize={12} />
-      </Button>
+    <VStack
+      px="8px"
+      minWidth="248px"
+      height="full"
+      key={list.id}
+      overflowY="auto"
+      pb="17px"
+      __css={{
+        '::-webkit-scrollbar': {
+          height: '5px',
+          width: '5px',
+        },
+        '::-webkit-scrollbar-thumb': {
+          background: 'gray.5',
+          borderRadius: 9999,
+        },
+        '::-webkit-scrollbar-thumb:hover': {
+          background: 'gray.4',
+        },
+      }}
+    >
+      <VStack spacing="28px" alignItems="flex-start" width="full">
+        <VStack spacing="17px" alignItems="flex-start" width="full">
+          <HStack width="full" justifyContent="space-between" alignItems="center">
+            <Heading color="gray.1" fontWeight={500} fontSize={14}>
+              {list.name}
+            </Heading>
+            <Box as="button" display="flex" alignItems="center">
+              <Icon as={MdMoreHoriz} fontSize={16} color="gray.3" />
+            </Box>
+          </HStack>
+          <Cards cards={list.cards} />
+        </VStack>
+        <Button width="full" variant="primary" style={{ background: '#DAE4FD', justifyContent: 'space-between', minHeight: '32px' }}>
+          <Text color="blue.1" fontWeight={500} fontSize={12} fontFamily={FontFamily.NotoSans}>
+            Add another card
+          </Text>
+          <Icon as={AiOutlinePlus} color="blue.1" fontSize={12} />
+        </Button>
+      </VStack>
     </VStack>
   );
 };
