8979b634721c09876868c78040cd06e331b374be
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-11 20:31:44 -0300

    feat(client): add reusable alert dialog component

---
+++ packages/client/src/components/AlertDialog/index.tsx
@@ -0,0 +1,49 @@
+import {
+  AlertDialog as ChakraAlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Text,
+} from '@chakra-ui/react';
+import { FC, ReactElement, useRef } from 'react';
+import Button from '../Button';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  subtitle: string;
+  actionButton: ReactElement;
+  loading?: boolean;
+};
+
+const AlertDialog: FC<Props> = ({ isOpen, onClose, title, subtitle, actionButton }) => {
+  const cancelRef = useRef(null);
+
+  return (
+    <ChakraAlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader color="gray.1" fontSize={16} fontWeight={500}>
+            {title}
+          </AlertDialogHeader>
+          <AlertDialogBody color="gray.3" fontSize={12} fontWeight={400}>
+            {subtitle}
+          </AlertDialogBody>
+          <AlertDialogFooter display="flex" justifyContent="flex-end" alignItems="center" gap={4}>
+            <Button variant="link" ref={cancelRef} onClick={onClose}>
+              <Text fontSize={12} color="gray.3" fontWeight={500}>
+                Cancel
+              </Text>
+            </Button>
+            {actionButton}
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </ChakraAlertDialog>
+  );
+};
+
+export default AlertDialog;
