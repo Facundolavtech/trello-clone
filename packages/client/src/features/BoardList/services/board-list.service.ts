e8a3c56ea7a716b2ff2d56f2d7aa9630fa7ea931
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-11 20:31:23 -0300

    feat(client): add update and delete board list services

--- packages/client/src/features/BoardList/services/board-list.service.ts
+++ packages/client/src/features/BoardList/services/board-list.service.ts
@@ -5,19 +5,40 @@ import { BoardList } from '../../../models/board-list.model';
 
 export interface ICreateListParams {
   boardId: string;
-  title: string;
+  name: string;
 }
 
 interface IGetAllBoardListsParams {
   boardId: string;
 }
 
-export async function createBoardList({ boardId, title }: ICreateListParams): Promise<BoardList> {
-  const response: AxiosResponse<BoardList> = await http.api.post(`${ApiRoutes.BOARD}/${boardId}/lists/create`, { name: title });
+export interface IUpdateListParams {
+  boardId: string;
+  listId: string;
+  name?: string;
+}
+
+export interface IDeleteListParams {
+  boardId: string;
+  listId: string;
+}
+
+export async function createBoardList({ boardId, name }: ICreateListParams): Promise<BoardList> {
+  const response: AxiosResponse<BoardList> = await http.api.post(`${ApiRoutes.BOARD}/${boardId}/lists/create`, { name });
   return response.data;
 }
 
 export async function getAllBoardLists({ boardId }: IGetAllBoardListsParams): Promise<BoardList[]> {
   const response: AxiosResponse<BoardList[]> = await http.api.get(`${ApiRoutes.BOARD}/${boardId}/lists`);
+  return response.data;
+}
+
+export async function updateList({ boardId, listId, ...update }: IUpdateListParams): Promise<BoardList> {
+  const response: AxiosResponse<BoardList> = await http.api.put(`${ApiRoutes.BOARD}/${boardId}/lists/update/${listId}`, { ...update });
+  return response.data;
+}
+
+export async function deleteList({ boardId, listId }: IDeleteListParams): Promise<BoardList> {
+  const response: AxiosResponse<BoardList> = await http.api.delete(`${ApiRoutes.BOARD}/${boardId}/lists/delete/${listId}`);
   return response.data;
 }
