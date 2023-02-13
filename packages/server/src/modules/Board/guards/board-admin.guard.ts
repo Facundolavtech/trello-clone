bc213efc316f0b915cca4e3f83136d4249c18425
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-13 20:29:49 -0300

    refactor(server): board admin guard for better readability

--- packages/server/src/modules/Board/guards/board-admin.guard.ts
+++ packages/server/src/modules/Board/guards/board-admin.guard.ts
@@ -1,20 +1,15 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { UnauthorizedException } from '@nestjs/common/exceptions';
-import { Board } from '../entities/Board.entity';
+import { WithBoardRequest } from '../interfaces';
 
 @Injectable()
 export class BoardAdminGuard implements CanActivate {
   constructor() {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const userId = request.user.id;
+    const request: WithBoardRequest = context.switchToHttp().getRequest();
 
-    const board: Board = request.board;
-
-    const isBoardAdmin = board.admin.id === userId;
-
-    if (!isBoardAdmin) {
+    if (request.board.admin.id !== request.user.id) {
       throw new UnauthorizedException('You need to be a board admin to perform this action');
     }
 
