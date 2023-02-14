587fcf36ae3463101b2df0e46c50f9697225f16f
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-14 00:31:06 -0300

    feat(client): add server side redirection based on auth cookie in app entry route

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,7 +1,20 @@
-import type { NextPage } from 'next';
+import type { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next';
+import config from '../config';
+import { AppRoutes } from '../config/routes';
 
 const Home: NextPage = () => {
   return null;
 };
 
 export default Home;
+
+export const getServerSideProps = (ctx: GetServerSidePropsContext): GetServerSidePropsResult<{}> => {
+  const cookie = ctx.req.cookies[config.Auth.CookieName];
+
+  return {
+    redirect: {
+      destination: `${cookie ? AppRoutes.DASHBOARD : AppRoutes.LOGIN}`,
+      permanent: false,
+    },
+  };
+};
