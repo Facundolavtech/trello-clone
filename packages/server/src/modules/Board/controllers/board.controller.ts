e8a193faf3cb596c7e2d4697d93d18428cf5ebe9
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-13 21:11:55 -0300

    refactor(server): delete board member methods from board controller, change find one method

--- packages/server/src/modules/Board/controllers/board.controller.ts
+++ packages/server/src/modules/Board/controllers/board.controller.ts
@@ -1,34 +1,17 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  UseGuards,
-  Put,
-  HttpCode,
-  HttpStatus,
-  BadRequestException,
-  Req,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards, Put, HttpCode, HttpStatus, BadRequestException, Req, NotFoundException } from '@nestjs/common';
 import { BoardService } from '../services/board.service';
 import { AuthenticatedGuard } from '../../Auth/guards/auth.guard';
-import { UserService } from '../../User/services/user.service';
 import { AuthenticatedRequest } from '../../../common/types';
 import { CreateBoardDTO } from '../dto/create.dto';
-import { HandleBoardMemberDTO, UpdateBoardDTO } from '../dto/update.dto';
+import { UpdateBoardDTO } from '../dto/update.dto';
 import { BoardAdminGuard } from '../guards/board-admin.guard';
-import { WithBoardRequest } from '../interfaces';
 import { BoardMemberGuard } from '../guards/board-member.guard';
 import { CustomUUIDPipe } from '../../../common/pipes/uuid.pipe';
 
 @UseGuards(AuthenticatedGuard)
 @Controller('boards')
 export class BoardController {
-  constructor(private readonly boardService: BoardService, private userService: UserService) {}
+  constructor(private readonly boardService: BoardService) {}
 
   @HttpCode(HttpStatus.CREATED)
   @UseGuards(AuthenticatedGuard)
@@ -53,19 +36,14 @@ export class BoardController {
     return await this.boardService.findAll(userId);
   }
 
+  @UseGuards(BoardMemberGuard)
   @HttpCode(HttpStatus.OK)
   @Get(':boardId')
-  async findOne(@Req() req: AuthenticatedRequest, @Param('boardId', CustomUUIDPipe) id: string) {
+  async findOne(@Param('boardId', CustomUUIDPipe) id: string) {
     const boardById = await this.boardService.findById(id, ['members', 'cards', 'cards.labels', 'cards.attachments', 'cards.members', 'cards.comments']);
 
     if (!boardById) {
       throw new NotFoundException('The board does not exists');
-    }
-
-    const userId = req.user.id;
-
-    if (!this.boardService.userIsBoardMember(boardById, userId) && boardById.isPrivate) {
-      throw new UnauthorizedException('You are not allowed to access this board');
     }
 
     return boardById;
@@ -75,65 +53,12 @@ export class BoardController {
   @UseGuards(BoardMemberGuard, BoardAdminGuard)
   @Put('update/:boardId')
   async update(@Param('boardId', CustomUUIDPipe) id: string, @Body() updateDTO: UpdateBoardDTO) {
+    const boardById = await this.boardService.findById(id);
+
+    if (!boardById) {
+      throw new NotFoundException('The board does not exists');
+    }
+
     return await this.boardService.update(id, updateDTO);
-  }
-
-  @HttpCode(HttpStatus.OK)
-  @UseGuards(BoardMemberGuard, BoardAdminGuard)
-  @Delete('delete/:boardId')
-  async remove(@Param('boardId', CustomUUIDPipe) id: string) {
-    return await this.boardService.delete(id);
-  }
-
-  @HttpCode(HttpStatus.OK)
-  @UseGuards(BoardMemberGuard, BoardAdminGuard)
-  @Post(':boardId/members/add')
-  async addMember(@Req() req: WithBoardRequest, @Body() handleMemberDTO: HandleBoardMemberDTO) {
-    const board = req.board;
-    const user = req.user;
-
-    if (user.id === handleMemberDTO.userId) {
-      throw new BadRequestException("You can't add yourself to board");
-    }
-
-    const userById = await this.userService.findById(handleMemberDTO.userId);
-
-    if (!userById) {
-      throw new NotFoundException('The user does not exists');
-    }
-
-    const userIsBoardMember = this.boardService.userIsBoardMember(board, handleMemberDTO.userId);
-
-    if (userIsBoardMember) {
-      throw new BadRequestException('User already be in the board');
-    }
-
-    return this.boardService.updateMembers(board.id, handleMemberDTO.userId, 'add');
-  }
-
-  @HttpCode(HttpStatus.OK)
-  @UseGuards(BoardMemberGuard, BoardAdminGuard)
-  @Delete(':boardId/members/delete')
-  async deleteMember(@Req() req: WithBoardRequest, @Body() handleMemberDTO: HandleBoardMemberDTO) {
-    const user = req.user;
-    const board = req.board;
-
-    if (user.id === handleMemberDTO.userId) {
-      throw new BadRequestException("You can't remove yourself from board");
-    }
-
-    const userById = await this.userService.findById(handleMemberDTO.userId);
-
-    if (!userById) {
-      throw new NotFoundException('The user does not exists');
-    }
-
-    const userIsBoardMember = this.boardService.userIsBoardMember(board, handleMemberDTO.userId);
-
-    if (!userIsBoardMember) {
-      throw new BadRequestException('User is not in the board');
-    }
-
-    return this.boardService.updateMembers(board.id, handleMemberDTO.userId, 'delete');
   }
 }
