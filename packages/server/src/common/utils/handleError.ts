2ef1f09011c97f9736ee29917b3c81b7f36bb34b
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-14 21:28:24 -0300

    refactor(server): remove unusued handle error utility function

--- packages/server/src/common/utils/handleError.ts
+++
@@ -1,9 +0,0 @@
-export default function handleError(error: any): void | null {
-  const debugMode: boolean = typeof process.env.DEBUG_MODE === 'string' && JSON.parse(process.env.DEBUG_MODE);
-
-  if (debugMode) {
-    return console.error(error.response ? error.response : error);
-  }
-
-  return null;
-}
