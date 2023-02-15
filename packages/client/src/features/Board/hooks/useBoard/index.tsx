6bc333ca7cb821805e50b11519c0d91256e0b273
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-14 21:41:40 -0300

    refactor(client): remove unnecesary async await in use board query

--- packages/client/src/features/Board/hooks/useBoard/index.tsx
+++ packages/client/src/features/Board/hooks/useBoard/index.tsx
@@ -4,7 +4,7 @@ import { IBoard } from '../../../../models/board.model';
 import { getBoardById } from '../../services/board.service';
 
 const useBoard = ({ id }: { id: string }) => {
-  return useQuery<IBoard, AxiosError<any>>([`board/${id}`], async () => await getBoardById(id));
+  return useQuery<IBoard, AxiosError<any>>([`board/${id}`], () => getBoardById(id));
 };
 
 export default useBoard;
