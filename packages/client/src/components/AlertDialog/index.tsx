bf998278e2e7f5932e75e2b9cbc2a9cdc6c9c259
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-16 23:56:43 -0300

    feat(client): add background opacity to alert dialog overlay

--- packages/client/src/components/AlertDialog/index.tsx
+++ packages/client/src/components/AlertDialog/index.tsx
@@ -24,24 +24,23 @@ const AlertDialog: FC<Props> = ({ isOpen, onClose, title, subtitle, actionButton
 
   return (
     <ChakraAlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
-      <AlertDialogOverlay>
-        <AlertDialogContent>
-          <AlertDialogHeader color="gray.1" fontSize={16} fontWeight={500}>
-            {title}
-          </AlertDialogHeader>
-          <AlertDialogBody color="gray.3" fontSize={12} fontWeight={400}>
-            {subtitle}
-          </AlertDialogBody>
-          <AlertDialogFooter display="flex" justifyContent="flex-end" alignItems="center" gap={4}>
-            <Button variant="link" ref={cancelRef} onClick={onClose}>
-              <Text fontSize={12} color="gray.3" fontWeight={500}>
-                Cancel
-              </Text>
-            </Button>
-            {actionButton}
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialogOverlay>
+      <AlertDialogOverlay backgroundColor="rgba(0,0,0,0.4)" />
+      <AlertDialogContent>
+        <AlertDialogHeader color="gray.1" fontSize={16} fontWeight={500}>
+          {title}
+        </AlertDialogHeader>
+        <AlertDialogBody color="gray.3" fontSize={12} fontWeight={400}>
+          {subtitle}
+        </AlertDialogBody>
+        <AlertDialogFooter display="flex" justifyContent="flex-end" alignItems="center" gap={4}>
+          <Button variant="link" ref={cancelRef} onClick={onClose}>
+            <Text fontSize={12} color="gray.3" fontWeight={500}>
+              Cancel
+            </Text>
+          </Button>
+          {actionButton}
+        </AlertDialogFooter>
+      </AlertDialogContent>
     </ChakraAlertDialog>
   );
 };
