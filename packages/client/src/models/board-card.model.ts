703a92ee1dfbb790d8caf498d42fb28193655036
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-16 23:54:25 -0300

    feat(client): add board card models

--- packages/client/src/models/board-card.model.ts
+++ packages/client/src/models/board-card.model.ts
@@ -1,1 +1,44 @@
-export interface IBoardCard {}
+import { IBoardMember } from './board.model';
+
+export interface IBoardCard {
+  id: string;
+  createdAt: number;
+  updatedAt: number;
+  title: string;
+  description: string | null;
+  cover: string | null;
+  boardId: string;
+  listId: string;
+  attachments: IBoardCardAttachment[];
+  labels: IBoardCardLabel[];
+  comments: IBoardCardComment[];
+  members: IBoardMember[];
+}
+
+export interface IBoardCardAttachment {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  url: string;
+  type: string;
+  cardId: string;
+}
+
+export interface IBoardCardLabel {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  color: string;
+  cardId: string;
+}
+
+export interface IBoardCardComment {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  content: string;
+  authorId: string;
+  cardId: string;
+}
