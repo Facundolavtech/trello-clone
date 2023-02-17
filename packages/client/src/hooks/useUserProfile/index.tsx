2283c7bf886cf372943e4db085b3d800fb5ea22b
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-16 23:55:37 -0300

    refactor(client): remove query options in use user profile hook

--- packages/client/src/hooks/useUserProfile/index.tsx
+++ packages/client/src/hooks/useUserProfile/index.tsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getUserProfile } from '../../services/user.service';
 
 const useUserProfile = () => {
-  return useQuery(['user/profile'], async () => await getUserProfile(), { enabled: false });
+  return useQuery(['user/profile'], getUserProfile);
 };
 
 export default useUserProfile;
