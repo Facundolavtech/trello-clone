9739f58d46a803e681a0a766292e90126ec7b436
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-20 21:48:43 -0300

    feat(client): add on card click function in card component

--- packages/client/src/features/BoardCard/components/Card/index.tsx
+++ packages/client/src/features/BoardCard/components/Card/index.tsx
@@ -1,21 +1,30 @@
-import { Avatar, color, Heading, HStack, Icon, Image, Text, Tooltip, VStack, Wrap, WrapItem } from '@chakra-ui/react';
+import { Avatar, Heading, HStack, Icon, Image, Text, Tooltip, VStack, Wrap, WrapItem } from '@chakra-ui/react';
+import NiceModal from '@ebay/nice-modal-react';
 import { FC } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { BsPaperclip } from 'react-icons/bs';
 import { MdInsertComment } from 'react-icons/md';
 import Button from '../../../../components/Button';
 import CustomCard from '../../../../components/Card';
+import { AppRoutes } from '../../../../config/routes';
 import { IBoardListCard } from '../../../../models/board-list.model';
 import { FontFamily } from '../../../../theme/constants';
 import capitalizeFirstLetter from '../../../../utils/capitalizeFirstLetter';
+import CardPage from '../Page';
 
 type Props = {
   card: IBoardListCard;
 };
 
 const Card: FC<Props> = ({ card }) => {
+  const onCardClick = () => {
+    if (typeof window === 'undefined') return;
+    NiceModal.show(CardPage, { cardId: card.id });
+    window.history.pushState(null, card.id, `${AppRoutes.BOARD}/${card.boardId}/c/${card.id}`);
+  };
+
   return (
-    <CustomCard variant="board-card" width="full">
+    <CustomCard variant="board-card" width="full" onClick={onCardClick}>
       <VStack spacing="12px" alignItems="flex-start">
         {card.cover && <Image src={card.cover} alt={`${card.title} cover`} width="full" height="130px" borderRadius="12px" objectFit="cover" />}
         <Heading fontSize={16} fontWeight={400} fontFamily={FontFamily.NotoSans}>
@@ -43,30 +52,32 @@ const Card: FC<Props> = ({ card }) => {
         </Wrap>
         <HStack width="full" justifyContent="space-between">
           <HStack spacing="12px">
-            <HStack spacing="8px">
-              {card.members
-                .sort((a, b) => a.createdAt - b.createdAt)
-                .slice(0, 2)
-                .map((member) => (
-                  <Tooltip key={member.id} label={member.user.name} aria-label="A tooltip" hasArrow bg="gray.4" color="white" fontWeight={400} placement="top">
-                    <Avatar
-                      size="sm"
-                      src={member.user.picture ?? ''}
-                      name={member.user.name}
-                      width="28px"
-                      height="28px"
-                      borderRadius="8px"
-                      bg={member.user.picture ? 'transparent' : 'gray.4'}
-                      color="white"
-                    />
-                  </Tooltip>
-                ))}
-              {card.members.length > 2 && (
-                <Text fontWeight={500} fontFamily={FontFamily.NotoSans} color="gray.4" fontSize={12}>
-                  +{card.members.length - 2}
-                </Text>
-              )}
-            </HStack>
+            {card.members.length > 0 && (
+              <HStack spacing="8px">
+                {card.members
+                  .sort((a, b) => a.createdAt - b.createdAt)
+                  .slice(0, 2)
+                  .map((member) => (
+                    <Tooltip key={member.id} label={member.user.name} aria-label="A tooltip" hasArrow bg="gray.4" color="white" fontWeight={400} placement="top">
+                      <Avatar
+                        size="sm"
+                        src={member.user.picture ?? ''}
+                        name={member.user.name}
+                        width="28px"
+                        height="28px"
+                        borderRadius="8px"
+                        bg={member.user.picture ? 'transparent' : 'gray.4'}
+                        color="white"
+                      />
+                    </Tooltip>
+                  ))}
+                {card.members.length > 2 && (
+                  <Text fontWeight={500} fontFamily={FontFamily.NotoSans} color="gray.4" fontSize={12}>
+                    +{card.members.length - 2}
+                  </Text>
+                )}
+              </HStack>
+            )}
             <Button variant="primary" width="28px" height="28px" px={0}>
               <Icon as={AiOutlinePlus} color="white" fontSize={14} />
             </Button>
