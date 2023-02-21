d728991131d50a25968335703f223c2ba908e662
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-20 21:52:36 -0300

    feat(client): add LoginSchema fields to RegisterSchema and adjust validation messages

--- packages/client/src/features/Auth/validations/index.ts
+++ packages/client/src/features/Auth/validations/index.ts
@@ -9,13 +9,9 @@ export const LoginSchema = Yup.object().shape({
 });
 
 export const RegisterSchema = Yup.object().shape({
+  ...LoginSchema.fields,
   name: Yup.string().max(50, 'The name must have a maximum of 50 characters').required('Name is required'),
   username: Yup.string().max(50, 'The username must have a maximum of 50 characters').required('Username is required'),
-  email: Yup.string().email('Invalid email address').required('Email address is required'),
-  password: Yup.string()
-    .min(6, 'The password must be at least 6 characters')
-    .max(50, 'The password must have a maximum of 50 characters')
-    .required('Password is required'),
   passwordConfirmation: Yup.string()
     .test('passwords-match', 'Password do not match', function (value) {
       return this.parent.password === value;
