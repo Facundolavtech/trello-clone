e0a8d1b675df73acbc2a4ef2c0c265372c49ed7f
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-20 21:50:21 -0300

    refactor(client): remove Divider from Dashboard Header and add it to Board Title

--- packages/client/src/components/Dashboard/Header/index.tsx
+++ packages/client/src/components/Dashboard/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Divider, HStack, Text, Icon, Box } from '@chakra-ui/react';
+import { HStack, Text, Icon, Box } from '@chakra-ui/react';
 import Searchbar from '../../../features/Board/components/Searchbar';
 import BoardTitle from '../../../features/Board/components/Title';
 import Button from '../../Button';
@@ -27,12 +27,11 @@ const Header = () => {
       alignItems="center"
     >
       <WrappedContainer styles={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Box gap={16} display="flex" height="full">
+        <Box gap="40px" display="flex" height="full">
           <Logo withTitle width="32px" height="29px" />
           {router.pathname.includes(AppRoutes.BOARD) && (
-            <HStack spacing={4} height="full">
+            <HStack spacing="24px" height="full">
               <BoardTitle />
-              <Divider orientation="vertical" height="50%" color="#E0E0E0" />
               <Link href={AppRoutes.DASHBOARD}>
                 <Button style={{ gap: 8 }} variant="lightgray" px={4} py={3}>
                   <Icon as={CgMenuGridR} width="12px" height="12px" color="gray.3" />
--- packages/client/src/features/Board/components/Title/index.tsx
+++ packages/client/src/features/Board/components/Title/index.tsx
@@ -1,4 +1,5 @@
-import { Heading } from '@chakra-ui/react';
+import { Box, Divider, Heading } from '@chakra-ui/react';
+import { HeaderStyles } from '../../../../theme/constants';
 import useBoard from '../../hooks/useBoard';
 import useBoardIdFromRoute from '../../hooks/useBoardIdFromRoute';
 import Loading from './Loading';
@@ -6,16 +7,23 @@ import Loading from './Loading';
 const BoardTitle = () => {
   const boardId = useBoardIdFromRoute();
 
-  const state = useBoard({ id: boardId });
+  const { error, isLoading, data: board } = useBoard({ id: boardId });
 
-  if (state.status === 'loading') {
+  if (error) {
+    return <Box width="60px" />;
+  }
+
+  if (isLoading || !board) {
     return <Loading />;
   }
 
   return (
-    <Heading color="gray.1" fontSize={18} fontWeight={500}>
-      {state.data?.title}
-    </Heading>
+    <>
+      <Heading color="gray.1" fontSize={18} fontWeight={500}>
+        {board.title}
+      </Heading>
+      <Divider orientation="vertical" height={(HeaderStyles.height * 50) / 100} color="#df4d4d" />
+    </>
   );
 };
 
