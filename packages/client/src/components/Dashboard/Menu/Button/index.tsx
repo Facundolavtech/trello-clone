2932f21edff018e179c2a3d5165d3020c5fb36ba
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-23 00:43:11 -0300

    refactor(client): update dashboard menu Button to include avatar component

--- packages/client/src/components/Dashboard/Menu/Button/index.tsx
+++ packages/client/src/components/Dashboard/Menu/Button/index.tsx
@@ -1,18 +1,21 @@
-import { Avatar, HStack, MenuButton as ChakraMenuButton, Text } from '@chakra-ui/react';
+import { HStack, MenuButton as ChakraMenuButton, Text } from '@chakra-ui/react';
 import { RiArrowDownSFill } from 'react-icons/ri';
 import { FontFamily } from '../../../../theme/constants';
 import useUserProfile from '../../../../hooks/useUserProfile';
+import Avatar from '../../../Avatar';
 
 const MenuButton = () => {
   const { data: user } = useUserProfile();
+
+  if (!user) return null;
 
   return (
     <ChakraMenuButton transition="all 0.2s">
       <HStack spacing="20px" alignItems="center">
         <HStack spacing="11px">
-          <Avatar width="32px" height="32px" size="sm" name={user?.name} src={user?.picture || ''} bg={user?.picture ? 'transparent' : 'gray.4'} color="white" />
+          <Avatar name={user.name} src={user.picture} />
           <Text color="gray.1" fontSize={12} fontWeight={700} fontFamily={FontFamily.NotoSans}>
-            {user?.name}
+            {user.name}
           </Text>
         </HStack>
         <RiArrowDownSFill color="gray.1" />
