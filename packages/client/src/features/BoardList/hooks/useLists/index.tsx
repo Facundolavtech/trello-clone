4a5289a4419234d391d5181fa3e74ab046a2694d
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-26 21:13:25 -0300

    refactor(client): change useLists hook to use getLists without async

--- packages/client/src/features/BoardList/hooks/useLists/index.tsx
+++ packages/client/src/features/BoardList/hooks/useLists/index.tsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getLists } from '../../services/list.service';
 
 const useLists = ({ boardId }: { boardId: string }) => {
-  return useQuery([`board/${boardId}/lists`], async () => await getLists({ boardId }));
+  return useQuery([`board/${boardId}/lists`], () => getLists({ boardId }));
 };
 
 export default useLists;
