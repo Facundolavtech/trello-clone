5e219a15c44c53dca8f1dd3bbafbc2e130492e2c
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-26 21:24:53 -0300

    feat(client): add custom BoardCanvas component

---
+++ packages/client/src/features/Board/components/Canvas/index.tsx
@@ -0,0 +1,36 @@
+import { FC, ReactNode } from 'react';
+import { Box } from '@chakra-ui/react';
+import useBoard from '../../hooks/useBoard';
+import useBoardIdFromRoute from '../../hooks/useBoardIdFromRoute';
+
+type Props = {
+  children: ReactNode;
+};
+
+const BoardCanvas: FC<Props> = ({ children }) => {
+  const boardId = useBoardIdFromRoute();
+
+  const { data: board } = useBoard({ id: boardId });
+
+  return (
+    <Box
+      display="flex"
+      width="100%"
+      gap="35px"
+      position="relative"
+      overflowX="scroll"
+      overflowY="hidden"
+      height={board ? 480 : 'auto'}
+      alignItems="flex-start"
+      backdropFilter="blur(10px)"
+      borderRadius="8px"
+      padding={8}
+      backgroundColor={board ? 'rgba(248, 248, 248, 0.7)' : 'none'}
+      className="custom__scrollbar"
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default BoardCanvas;
