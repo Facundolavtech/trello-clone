81120ac426bbf583e4a9de5f5eef1e171d29cc4f
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-26 21:14:46 -0300

    refactor(client): change skeleton height in loading component

--- packages/client/src/features/BoardList/components/Lists/Loading.tsx
+++ packages/client/src/features/BoardList/components/Lists/Loading.tsx
@@ -4,7 +4,7 @@ const Loading = () => {
   return (
     <>
       {new Array(4).fill(4).map((_, i) => {
-        return <Skeleton key={i} borderRadius="8px" width={{ base: 'full', md: '240px' }} height="20px" />;
+        return <Skeleton key={i} borderRadius="8px" width={{ base: 'full', md: '240px' }} height="30px" />;
       })}
     </>
   );
