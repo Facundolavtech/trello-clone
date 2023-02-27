f9a9cd963e9bbeb36af9b12909d6b3060fdb6937
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-26 21:24:33 -0300

    feat(client): add AddButton to BoardMembers component

--- packages/client/src/features/Board/components/Members/index.tsx
+++ packages/client/src/features/Board/components/Members/index.tsx
@@ -1,11 +1,11 @@
 import { HStack } from '@chakra-ui/react';
+import AddButton from '../../../../components/Buttons/Add';
 import MemberList from '../../../../components/Members';
 import useUserProfile from '../../../../hooks/useUserProfile';
 import sortArr from '../../../../utils/sortArr';
 import useBoard from '../../hooks/useBoard';
 import useBoardIdFromRoute from '../../hooks/useBoardIdFromRoute';
 import userIsBoardAdmin from '../../utils/userIsBoardAdmin';
-import AddBoardMemberButton from '../Buttons/AddMember';
 import Loading from './Loading';
 
 const BoardMembers = () => {
@@ -22,8 +22,8 @@ const BoardMembers = () => {
 
   return (
     <HStack spacing="16px">
-      <MemberList members={sortArr(board.members, 'createdAt')} maxMembers={4} useTooltip={true} />
-      {userIsBoardAdmin(board.admin.id, user.id) && <AddBoardMemberButton />}
+      <MemberList members={sortArr(board.members, 'createdAt')} maxMembers={4} />
+      {userIsBoardAdmin(board.admin.id, user.id) && <AddButton onClick={() => null} />}
     </HStack>
   );
 };
