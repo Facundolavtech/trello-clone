0e9bed750b13532c70eac45e33b574f7cd03c949
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-26 23:00:15 -0300

    feat(client): add BoardMenu Header

---
+++ packages/client/src/features/Board/components/Menu/Header/index.tsx
@@ -0,0 +1,23 @@
+import { Box, Divider, Heading, HStack, Icon, VStack } from '@chakra-ui/react';
+import { MdClose } from 'react-icons/md';
+import { useBoardContext } from '../../../context/board';
+
+const Header = () => {
+  const { onClose } = useBoardContext();
+
+  return (
+    <VStack spacing="8px" width="full">
+      <HStack width="full" justifyContent="space-between">
+        <Heading fontWeight={600} fontSize={12} color="gray.1">
+          Menu
+        </Heading>
+        <Box as="button" _hover={{ cursor: 'pointer' }} onClick={onClose}>
+          <Icon as={MdClose} color="gray.2" fontSize={18} />
+        </Box>
+      </HStack>
+      <Divider width="full" orientation="horizontal" borderColor="gray.5" />
+    </VStack>
+  );
+};
+
+export default Header;
