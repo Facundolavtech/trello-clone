cd15ffc4faf2f23d2c1896fc1547ec0a2830623b
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-26 21:19:18 -0300

    feat(client): add useCardList hook to BoardCard feature

---
+++ packages/client/src/features/BoardCard/hooks/useCardList/index.tsx
@@ -0,0 +1,21 @@
+import { useMemo } from 'react';
+import useLists from '../../../BoardList/hooks/useLists';
+import useCard from '../useCard';
+
+type Props = {
+  cardId: string;
+  boardId: string;
+};
+
+const useCardList = ({ cardId, boardId }: Props) => {
+  const { data: card } = useCard({ id: cardId });
+  const { data: lists } = useLists({ boardId });
+
+  return useMemo(() => {
+    if (!lists || !card) return null;
+
+    return lists.find((l) => l.id === card.listId);
+  }, [card]);
+};
+
+export default useCardList;
