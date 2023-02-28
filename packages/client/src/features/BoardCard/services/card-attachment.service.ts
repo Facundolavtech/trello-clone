9ef82dc1d1dd0664220b31498d760f411c1e6136
Facundo Lavagnino <flavagnino8@gmail.com>
2023-02-28 20:30:14 -0300

    feat(client): add card attachment service

--- packages/client/src/features/BoardCard/services/card-attachment.service.ts
+++ packages/client/src/features/BoardCard/services/card-attachment.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import fileDownload from 'js-file-download';
 
-export default async function downloadAttachment(url: string, filename: string) {
+export async function download(url: string, filename: string) {
   const response = await axios.get(url, {
     responseType: 'blob',
   });
