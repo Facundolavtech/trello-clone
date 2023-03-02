65c96654371588b472d389f6c0118929141e171d
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-02 00:55:46 -0300

    feat(client): add upload and delete attachment services

--- packages/client/src/features/BoardCard/services/card-attachment.service.ts
+++ packages/client/src/features/BoardCard/services/card-attachment.service.ts
@@ -1,9 +1,45 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import fileDownload from 'js-file-download';
+import http from '../../../config/http';
+import { IBoardCardAttachment } from '../../../models/board-card.model';
 
-export async function download(url: string, filename: string) {
+export interface IUploadAttachmentParams {
+  boardId: string;
+  cardId: string;
+  file: File;
+}
+
+export interface IDeleteAttachmentParams {
+  boardId: string;
+  cardId: string;
+  attachmentId: string;
+}
+
+export async function downloadAttachment(url: string, filename: string): Promise<void> {
   const response = await axios.get(url, {
     responseType: 'blob',
   });
+
   fileDownload(response.data, filename);
+}
+
+export async function uploadAttachment(params: IUploadAttachmentParams): Promise<IBoardCardAttachment> {
+  let formData = new FormData();
+  formData.append('file', params.file, params.file.name);
+
+  const response: AxiosResponse<IBoardCardAttachment> = await http.api.post(`boards/${params.boardId}/cards/${params.cardId}/attachments/upload`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  return response.data;
+}
+
+export async function deleteAttachment(params: IDeleteAttachmentParams): Promise<IBoardCardAttachment> {
+  const response: AxiosResponse<IBoardCardAttachment> = await http.api.delete(
+    `boards/${params.boardId}/cards/${params.cardId}/attachments/delete/${params.attachmentId}`
+  );
+
+  return response.data;
 }
