9cda3eccb5966d82b7a9399aa3ca1a253a35ae0a
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-02 00:54:19 -0300

    refactor(client): update NextNProgress to showOnShallow false and use chakra blue color

--- packages/client/src/libs/NextProgress/index.tsx
+++ packages/client/src/libs/NextProgress/index.tsx
@@ -1,7 +1,7 @@
 import NextNProgress from 'nextjs-progressbar';
 
 const NextProgress = () => {
-  return <NextNProgress color="#2F80ED" height={2} startPosition={0.4} options={{ easing: 'ease', speed: 500 }} />;
+  return <NextNProgress showOnShallow={false} color="var(--chakra-colors-blue-1)" height={2} startPosition={0.4} options={{ easing: 'ease', speed: 500 }} />;
 };
 
 export default NextProgress;
