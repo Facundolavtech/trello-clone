6c57a6a33e51053f650b256e48a5a3c4a941bf92
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-02 01:02:17 -0300

    refactor(client): remove unnecesary onError callback function in update board mutation

--- packages/client/src/features/Board/hooks/useUpdateBoard/index.tsx
+++ packages/client/src/features/Board/hooks/useUpdateBoard/index.tsx
@@ -1,6 +1,4 @@
-import { useToast } from '@chakra-ui/react';
 import { useIsMutating, useMutation, useQueryClient } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
 import { IBoard } from '../../../../models/board.model';
 import updateQueryData from '../../../../utils/updateQueryData';
 import { IUpdateBoardParams, updateBoard } from '../../services/board.service';
@@ -9,23 +7,10 @@ import useBoardIdFromRoute from '../useBoardIdFromRoute';
 const useUpdateBoard = () => {
   const boardId = useBoardIdFromRoute();
   const queryClient = useQueryClient();
-  const toast = useToast();
 
   const mutation = useMutation((params: IUpdateBoardParams) => updateBoard(params), {
     mutationKey: [`board/${boardId}/update`],
     onSuccess: (data: IBoard) => onSuccess(data),
-    onError: (err: AxiosError<any>) => {
-      if (err.response?.data.code !== 'TokenExpiredError') {
-        toast({
-          position: 'top-right',
-          duration: 2000,
-          isClosable: false,
-          status: 'error',
-          title: 'Error',
-          description: err.response?.data.message || 'An error occurred while trying to update the board',
-        });
-      }
-    },
   });
 
   const onSuccess = (board: IBoard) => {
