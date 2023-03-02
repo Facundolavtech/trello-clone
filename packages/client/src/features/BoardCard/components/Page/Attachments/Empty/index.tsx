17b205aa0291f6e73f32d0de5d2c19a81807a439
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-02 01:03:58 -0300

    feat(client): add empty attachments message

---
+++ packages/client/src/features/BoardCard/components/Page/Attachments/Empty/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@chakra-ui/react';
+
+const Empty = () => {
+  return (
+    <Text color="gray.4" fontSize={10}>
+      There are no attachments
+    </Text>
+  );
+};
+
+export default Empty;
