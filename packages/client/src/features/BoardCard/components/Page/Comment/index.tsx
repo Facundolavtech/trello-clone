7a97815b658e012bb5c7a0cded00d2d8024563b4
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-08 03:40:43 -0300

    refactor(client): comment component to add CommentContextProvider and useUserIsCommentAuthor hook

--- packages/client/src/features/BoardCard/components/Page/Comment/index.tsx
+++ packages/client/src/features/BoardCard/components/Page/Comment/index.tsx
@@ -1,30 +1,29 @@
-import { FC, useMemo } from 'react';
+import { FC } from 'react';
 import { HStack, VStack } from '@chakra-ui/react';
-import useUserProfile from '../../../../../hooks/useUserProfile';
 import { IBoardCardComment } from '../../../../../models/board-card.model';
 import Actions from './Actions';
 import Content from './Content';
 import Information from './Information';
+import useUserIsCommentAuthor from '../../../hooks/useUserIsCommentAuthor';
+import CommentContextProvider from './Context';
 
 type Props = {
   comment: IBoardCardComment;
 };
 
 const Comment: FC<Props> = ({ comment }) => {
-  const { data: user } = useUserProfile();
-
-  const userIsCommentAuthor = useMemo(() => {
-    return comment.author.user.id === user?.id;
-  }, [user, comment]);
+  const userIsCommentAuthor = useUserIsCommentAuthor({ comment });
 
   return (
-    <VStack width="full" spacing="13px">
-      <HStack width="full" justifyContent="space-between">
-        <Information author={comment.author} createdDate={comment.createdAt} />
-        {userIsCommentAuthor && <Actions commentId={comment.id} />}
-      </HStack>
-      <Content content={comment.content} />
-    </VStack>
+    <CommentContextProvider id={comment.id}>
+      <VStack width="full" spacing="13px" maxWidth="full">
+        <HStack width="full" justifyContent="space-between">
+          <Information author={comment.author} createdDate={comment.createdAt} />
+          {userIsCommentAuthor && <Actions id={comment.id} content={comment.content} />}
+        </HStack>
+        <Content content={comment.content} />
+      </VStack>
+    </CommentContextProvider>
   );
 };
 
