9b316ab4d6b7c6dd2b859f7b3e1ac9d84ca6689a
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-08 03:44:56 -0300

    refactor(client): update font size and text for DescriptionContent component

--- packages/client/src/features/BoardCard/components/Page/Description/Content/index.tsx
+++ packages/client/src/features/BoardCard/components/Page/Description/Content/index.tsx
@@ -2,8 +2,8 @@ import { Text } from '@chakra-ui/react';
 
 const DescriptionContent = ({ description }) => {
   return (
-    <Text fontSize={14} fontWeight={400} color={description ? 'black' : 'gray.4'}>
-      {description ? description : 'Enter new description...'}
+    <Text fontSize={description ? 14 : 12} fontWeight={400} color={description ? 'black' : 'gray.4'}>
+      {description ? description : 'Enter new description'}
     </Text>
   );
 };
