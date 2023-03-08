a870586a2026bc7c0c0e70a7dd2c79141a316374
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-08 03:37:44 -0300

    feat(client): add updateComment and deleteComment services for BoardCard comments

--- packages/client/src/features/BoardCard/services/card-comment.service.ts
+++ packages/client/src/features/BoardCard/services/card-comment.service.ts
@@ -8,10 +8,34 @@ export interface ICreateCommentParams {
   content: string;
 }
 
+export interface IUpdateCommentParams extends ICreateCommentParams {
+  id: string;
+}
+
+export interface IDeleteCommentParams {
+  boardId: string;
+  cardId: string;
+  id: string;
+}
+
 export async function createComment(params: ICreateCommentParams) {
   const response: AxiosResponse<IBoardCardComment> = await http.api.post(`boards/${params.boardId}/cards/${params.cardId}/comments/create`, {
     content: params.content,
   });
+
+  return response.data;
+}
+
+export async function updateComment(params: IUpdateCommentParams) {
+  const response: AxiosResponse<IBoardCardComment> = await http.api.put(`boards/${params.boardId}/cards/${params.cardId}/comments/update/${params.id}`, {
+    content: params.content,
+  });
+
+  return response.data;
+}
+
+export async function deleteComment(params: IDeleteCommentParams) {
+  const response: AxiosResponse<IBoardCardComment> = await http.api.delete(`boards/${params.boardId}/cards/${params.cardId}/comments/delete/${params.id}`);
 
   return response.data;
 }
