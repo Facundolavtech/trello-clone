1e607307366b781aed8cfe8e62cfb0af95cc41ef
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-08 03:45:44 -0300

    refactor(client): update text for LabelMenu Button to read "Labels"

--- packages/client/src/features/BoardCard/components/Page/Sidepanel/Actions/LabelMenu/Buttons/Open/index.tsx
+++ packages/client/src/features/BoardCard/components/Page/Sidepanel/Actions/LabelMenu/Buttons/Open/index.tsx
@@ -8,7 +8,7 @@ const OpenButton = () => {
       <Button as="div" height="32px" variant="lightgray" style={{ gap: '13px', justifyContent: 'flex-start' }}>
         <Icon as={MdLabel} color="gray.3" fontSize={12} />
         <Text color="gray.3" fontSize={12} fontWeight={500}>
-          Label
+          Labels
         </Text>
       </Button>
     </ChakraMenuButton>
