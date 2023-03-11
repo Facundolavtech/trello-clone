7511e40299ad269a3665cd1cd885bd1fb527ea49
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-11 00:03:24 -0300

    refactor(client): remove AxiosError type from useBoards hook

--- packages/client/src/features/Board/hooks/useBoards/index.tsx
+++ packages/client/src/features/Board/hooks/useBoards/index.tsx
@@ -1,10 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
 import { IBoard } from '../../../../models/board.model';
 import { getBoards } from '../../services/board.service';
 
 const useBoards = () => {
-  return useQuery<IBoard[], AxiosError<any>>([`boards/all`], async () => getBoards());
+  return useQuery<IBoard[]>([`boards/all`], getBoards);
 };
 
 export default useBoards;
