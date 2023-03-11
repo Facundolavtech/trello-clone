eef1a25b6dfabe6d44cbf89a47b8eb0cbfc09314
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-11 00:00:13 -0300

    refactor(client): rename CoverMenu to Cover

--- packages/client/src/features/BoardCard/components/Page/Sidepanel/Actions/Cover/Buttons/Open/index.tsx
+++ packages/client/src/features/BoardCard/components/Page/Sidepanel/Actions/Cover/Buttons/Open/index.tsx
@@ -6,8 +6,8 @@ const OpenButton = () => {
   return (
     <ChakraMenuButton width="full">
       <Button as="div" height="32px" variant="lightgray" style={{ gap: '13px', justifyContent: 'flex-start' }}>
-        <Icon as={AiFillPicture} color="gray.3" fontSize={12} />
-        <Text color="gray.3" fontSize={12} fontWeight={500}>
+        <Icon as={AiFillPicture} color="gray.3" fontSize={{ base: 10, md: 12 }} />
+        <Text color="gray.3" fontSize={{ base: 10, md: 12 }} fontWeight={500}>
           Cover
         </Text>
       </Button>
