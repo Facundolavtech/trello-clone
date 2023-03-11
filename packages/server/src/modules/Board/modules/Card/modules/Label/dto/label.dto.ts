bd0d0c510339ba38fa77c78526e82b87c9114117
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-11 02:33:01 -0300

    feat(server): add min length validation to CreateCardLabelDTO

--- packages/server/src/modules/Board/modules/Card/modules/Label/dto/label.dto.ts
+++ packages/server/src/modules/Board/modules/Card/modules/Label/dto/label.dto.ts
@@ -1,10 +1,11 @@
-import { IsHexColor, IsIn, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsHexColor, IsIn, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 const availableColors = ['#219653', '#F2C94C', '#F2994A', '#EB5757', '#2F80ED', '#56CCF2', '#6FCF97', '#333333', '#4F4F4F', '#828282', '#BDBDBD', '#E0E0E0'];
 
 export class CreateCardLabelDTO {
   @IsNotEmpty({ message: 'Error: Card label name required' })
   @IsString({ message: 'Error: Card label name is not valid' })
+  @MinLength(4, { message: 'Error: Card label name must contain a minimum of 4 characters' })
   @MaxLength(12, { message: 'Error: Card label name must contain a maximum of 12 characters' })
   name: string;
 
