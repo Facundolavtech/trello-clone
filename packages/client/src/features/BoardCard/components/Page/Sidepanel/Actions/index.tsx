935e2f8ad55351fbf7abfa553ef03673a5c82728
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-11 00:00:44 -0300

    refactor(client): update VStack component to Flex

--- packages/client/src/features/BoardCard/components/Page/Sidepanel/Actions/index.tsx
+++ packages/client/src/features/BoardCard/components/Page/Sidepanel/Actions/index.tsx
@@ -1,15 +1,15 @@
-import { VStack } from '@chakra-ui/react';
-import CoverMenu from './CoverMenu';
-import LabelMenu from './LabelMenu';
+import { Flex } from '@chakra-ui/react';
+import CoverMenu from './Cover';
+import LabelMenu from './Labels';
 import Title from './Title';
 
 const Actions = () => {
   return (
-    <VStack spacing="12px" width="full" alignItems="flex-start">
+    <Flex width="full" gap="12px" flexDirection="column" alignItems="flex-start">
       <Title />
       <CoverMenu />
       <LabelMenu />
-    </VStack>
+    </Flex>
   );
 };
 
