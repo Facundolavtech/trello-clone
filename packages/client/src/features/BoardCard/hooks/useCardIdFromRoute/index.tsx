fe9f6d58f4945b90f43eb8391208931eb19f61a0
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-15 17:29:22 -0300

    refactor(client): delete unusued useCardIdFromRoute hook

--- packages/client/src/features/BoardCard/hooks/useCardIdFromRoute/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-const useCardIdFromRoute = () => {
-  const router = useRouter();
-
-  return router.query.cardId as string;
-};
-
-export default useCardIdFromRoute;
