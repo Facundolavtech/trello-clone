30876201c12e6f2e74292e52275db5215a6e1032
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-15 05:39:47 -0300

    feat(client): add background image to BoardPage based on board cover

--- packages/client/src/features/Board/components/Page/index.tsx
+++ packages/client/src/features/Board/components/Page/index.tsx
@@ -7,30 +7,45 @@ import ShowBoardMenuButton from '../Buttons/ShowMenu';
 import Lists from '../../../BoardList/components/Lists';
 import BoardMenu from '../Menu';
 import BoardCanvas from '../Canvas';
+import BoardContextWrapper from '../../context/board';
+import useBoard from '../../hooks/useBoard';
+import useBoardIdFromRoute from '../../hooks/useBoardIdFromRoute';
 
-type Props = {};
+const BoardPage = () => {
+  const boardId = useBoardIdFromRoute();
 
-const BoardPage: FC<Props> = () => {
+  const { data: board } = useBoard({ id: boardId });
+
   return (
-    <BoardLayout>
-      <BoardMenu />
-      <Stack width="full" mt="35px">
-        <Stack width="full" direction={{ base: 'column', md: 'row' }} mb={10}>
-          <VStack width="full" alignItems="flex-start" spacing="52px">
-            <HStack width="full" justifyContent="space-between" as="section">
-              <Flex gap="19px" width="full">
-                <BoardPrivacy />
-                <BoardMembers />
-              </Flex>
-              <ShowBoardMenuButton />
-            </HStack>
-            <BoardCanvas>
-              <Lists />
-            </BoardCanvas>
-          </VStack>
+    <BoardContextWrapper>
+      <style jsx global>{`
+        body {
+          background-image: url(${board?.cover});
+          background-repeat: no-repeat;
+          background-size: cover;
+          background-attachment: fixed;
+        }
+      `}</style>
+      <BoardLayout>
+        <BoardMenu />
+        <Stack width="full" mt="35px">
+          <Stack width="full" direction={{ base: 'column', md: 'row' }} mb={10}>
+            <VStack width="full" alignItems="flex-start" spacing="52px">
+              <HStack width="full" justifyContent="space-between" as="section">
+                <Flex gap="19px" width="full">
+                  <BoardPrivacy />
+                  <BoardMembers />
+                </Flex>
+                <ShowBoardMenuButton />
+              </HStack>
+              <BoardCanvas>
+                <Lists />
+              </BoardCanvas>
+            </VStack>
+          </Stack>
         </Stack>
-      </Stack>
-    </BoardLayout>
+      </BoardLayout>
+    </BoardContextWrapper>
   );
 };
 
--- packages/client/src/features/Board/layouts/BoardLayout/index.tsx
+++ packages/client/src/features/Board/layouts/BoardLayout/index.tsx
@@ -1,6 +1,5 @@
 import { FC, ReactNode } from 'react';
 import WrappedContainer from '../../../../components/Containers/Wrapped';
-import SEO from '../../../../components/SEO';
 import DashboardLayout from '../../../../layout/Dashboard';
 import BoardPageError from '../../components/Page/Error';
 import useBoard from '../../hooks/useBoard';
@@ -13,11 +12,10 @@ type Props = {
 const BoardLayout: FC<Props> = ({ children }) => {
   const boardId = useBoardIdFromRoute();
 
-  const { data: board, error } = useBoard({ id: boardId });
+  const { error } = useBoard({ id: boardId });
 
   return (
     <DashboardLayout>
-      <SEO title={board?.title} />
       <WrappedContainer>{error ? <BoardPageError error={error} /> : children}</WrappedContainer>
     </DashboardLayout>
   );
