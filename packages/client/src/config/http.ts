ccc572e6391e2890ddbcdc2333180e8fe6f9b93a
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-15 05:40:51 -0300

    refactor(client): remove toast from axios interceptor

--- packages/client/src/config/http.ts
+++ packages/client/src/config/http.ts
@@ -1,11 +1,8 @@
 import config from '.';
 import axios from 'axios';
-import { createStandaloneToast } from '@chakra-ui/react';
 import { AppRoutes } from './routes';
 import { getCookie } from 'cookies-next';
 import { deleteSessionCookie } from '../utils/sessionCookie';
-
-const { toast } = createStandaloneToast();
 
 const http = {
   api: axios.create({
@@ -19,6 +16,7 @@ let isHandlingError = false;
 
 http.api.interceptors.request.use((cfg) => {
   const token = getCookie(config.AUTH.COOKIE_NAME);
+
   if (token) {
     cfg.headers['Authorization'] = `Bearer ${token}`;
   } else {
@@ -41,15 +39,6 @@ http.api.interceptors.response.use(
 
     if (error.response?.status === 401 && error.response?.data.code === 'TokenExpiredError') {
       deleteSessionCookie();
-
-      toast({
-        position: 'top-right',
-        title: 'Error',
-        description: error.response.data.message,
-        status: 'error',
-        duration: 2000,
-        isClosable: false,
-      });
 
       if (typeof window !== 'undefined') {
         window.location.href = AppRoutes.LOGIN;
