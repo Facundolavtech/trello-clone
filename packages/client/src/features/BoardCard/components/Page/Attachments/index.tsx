a887f60eefe8d094b62a40a49d2ff30b796e2ea3
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-16 18:42:27 -0300

    feat(client): add maxHeight, pr, overflowY, and custom scrollbar to Attachments component

--- packages/client/src/features/BoardCard/components/Page/Attachments/index.tsx
+++ packages/client/src/features/BoardCard/components/Page/Attachments/index.tsx
@@ -12,12 +12,28 @@ type Props = {
 
 const Attachments: FC<Props> = ({ attachments }) => {
   return (
-    <VStack width="full" alignItems="flex-start" spacing="14px" as="section">
-      <HStack>
+    <VStack
+      width="full"
+      alignItems="flex-start"
+      as="section"
+      maxHeight="300px"
+      pr="10px"
+      overflowY="auto"
+      className="custom__scrollbar"
+      __css={{
+        '::-webkit-scrollbar': {
+          height: '5px',
+          width: '5px',
+        },
+      }}
+    >
+      <HStack mb="14px">
         <Title />
         <Upload />
       </HStack>
-      {attachments.length === 0 ? <Empty /> : attachments.map((attachment) => <Attachment attachment={attachment} key={attachment.id} />)}
+      <VStack width="full" alignItems="flex-start" spacing="14px">
+        {attachments.length === 0 ? <Empty /> : attachments.map((attachment) => <Attachment attachment={attachment} key={attachment.id} />)}
+      </VStack>
     </VStack>
   );
 };
