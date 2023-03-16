f8146b98575d8fa49e777358a5cf74368784d492
Facundo Lavagnino <flavagnino8@gmail.com>
2023-03-16 01:16:24 -0300

    refactor(server): update guard to throw ExpiredTokenException instead of TokenExpiredException

--- packages/server/src/modules/Auth/guards/auth.guard.ts
+++ packages/server/src/modules/Auth/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
-import { TokenExpiredException } from '../../../common/exceptions/unauthorized.exceptions';
+import { ExpiredTokenException } from '../../../common/exceptions/unauthorized.exceptions';
 
 @Injectable()
 export class AuthenticatedGuard extends AuthGuard('jwt') {
   handleRequest(err, user) {
     if (err || !user) {
-      throw err || new TokenExpiredException();
+      throw err || new ExpiredTokenException();
     }
     return user;
   }
