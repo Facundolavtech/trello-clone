bedc1fd759a79fdda8ec8ea831ec206722eb203e
Facundo Lavagnino <flavagnino8@gmail.com>
2023-04-03 00:33:59 -0300

    refactor: update Logo proptypes

--- packages/client/src/components/Logo/index.tsx
+++ packages/client/src/components/Logo/index.tsx
@@ -3,16 +3,15 @@ import Image from 'next/image';
 import { HStack } from '@chakra-ui/react';
 
 interface Props {
-  width: number | string;
-  height: number | string;
-  src: string;
-  children: ReactNode;
+  width?: number | string;
+  height?: number | string;
+  children?: ReactNode;
 }
 
-const Logo: FC<Props> = ({ width, height, src, children }) => {
+const Logo: FC<Props> = ({ width, height, children }) => {
   return (
     <HStack alignItems="center" spacing={4}>
-      <Image priority objectFit="contain" width={width} height={height} alt="Logo" src={src} />
+      <Image priority objectFit="contain" width={width} height={height} alt="Logo" src="/assets/svg/logo.svg" />
       {children}
     </HStack>
   );
