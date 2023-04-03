d8bab0bef4753a81310952d6771734c5a3e11ff1
Facundo Lavagnino <flavagnino8@gmail.com>
2023-04-03 00:02:36 -0300

    refactor: remove unusued LinkItem, add container, add children to MenuItems

--- packages/client/src/components/Dashboard/Menu/Items/index.tsx
+++ packages/client/src/components/Dashboard/Menu/Items/index.tsx
@@ -1,23 +1,21 @@
-import { ReactElement } from 'react';
-import Link from 'next/link';
-import { FiLogOut } from 'react-icons/fi';
+import { Icon, Text, VStack } from '@chakra-ui/react';
 import MenuItem from 'components/Dashboard/Menu/Item';
 import useAuthMethods from 'features/Auth/hooks/useAuthMethods';
+import { FiLogOut } from 'react-icons/fi';
 
 const MenuItems = () => {
   const { logout } = useAuthMethods();
 
   return (
-    <MenuItem icon={FiLogOut} content="Logout" bg="red.400" styles={{ borderRadius: 8, fontWeight: 500, fontSize: 14 }} color="white" onClick={() => logout()} />
+    <VStack>
+      <MenuItem height="40px" _hover={{ bg: 'red.400' }} bg="red.500" borderRadius="8px" color="white" onClick={() => logout()}>
+        <Icon as={FiLogOut} color="white" />
+        <Text fontWeight={500} color="white" fontSize={14}>
+          Logout
+        </Text>
+      </MenuItem>
+    </VStack>
   );
 };
 
 export default MenuItems;
-
-const LinkItem = ({ path, children }: { path: string; children: ReactElement | ReactElement[] }) => {
-  if (window?.location.href === path) {
-    return <>{children}</>;
-  }
-
-  return <Link href={path}>{children}</Link>;
-};
