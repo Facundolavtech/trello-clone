910f3f93872566ec29d9ef11f765a79d0e745346
Facundo Lavagnino <flavagnino8@gmail.com>
2023-04-02 23:55:05 -0300

    refactor: update CardAttachment structure

--- packages/client/src/features/BoardCard/components/Page/Attachments/Attachment/index.tsx
+++ packages/client/src/features/BoardCard/components/Page/Attachments/Attachment/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Hide, HStack, VStack } from '@chakra-ui/react';
+import { Box, Hide, HStack, VStack } from '@chakra-ui/react';
 import Information from 'features/BoardCard/components/Page/Attachments/Attachment/Information';
 import Cover from 'features/BoardCard/components/Page/Attachments/Attachment/Cover';
 import Actions from 'features/BoardCard/components/Page/Attachments/Attachment/Actions';
@@ -11,14 +11,9 @@ type Props = {
 
 const Attachment: FC<Props> = ({ attachment }) => {
   return (
-    <HStack as="article" width="full" justifyContent="flex-start" spacing="13px" alignItems="flex-start">
-      <Hide breakpoint="(max-width: 580px)">
-        <Cover name={attachment.name} type={attachment.type} url={attachment.url} />
-      </Hide>
-      <VStack spacing="6px" width="full" alignItems="flex-start">
-        <Hide breakpoint="(min-width: 580px)">
-          <Cover name={attachment.name} type={attachment.type} url={attachment.url} />
-        </Hide>
+    <HStack width="full" justifyContent="flex-start" spacing="13px" alignItems="flex-start">
+      <Cover name={attachment.name} type={attachment.type} url={attachment.url} />
+      <VStack spacing="6px" alignItems="flex-start">
         <Information attachment={attachment} />
         <Actions attachment={attachment} />
       </VStack>
